53835ff001e476bb66dabe60e9a57d42692e6bda
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2022-06-10 00:01:04 +0530

    Time: 2240 ms (36.21%), Space: 38.6 MB (8.51%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,13 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = {}
+        
+        def check(amount):
+            if amount in memo: return memo[amount]
+            if amount == 0: return 0
+            if amount < 0: return float("inf")
+            memo[amount] = min([1 + check(amount - coin) for coin in coins])
+            return memo[amount]
+        
+        minimum = check(amount)
+        return minimum if minimum < float("inf") else -1

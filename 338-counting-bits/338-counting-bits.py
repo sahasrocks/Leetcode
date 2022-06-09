8c8ae7e1a89439e4b54b97fa3846e55acf9151a4
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2022-06-10 00:34:45 +0530

    Time: 120 ms (58.63%), Space: 20.9 MB (28.39%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        res = [0]
+        while len(res) <= num:
+            res += [i+1 for i in res]
+        return res[:num+1]
+        

022bbcf9efc1050ba20021538fb93a13f5f08227
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2022-08-25 15:30:40 +0530

    Time: 53 ms (50.41%), Space: 14 MB (9.62%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -7,18 +7,18 @@ class Solution:
         
 #         else:
 #             return self.fib(n-1)+self.fib(n-2)
-#     def fib(self, n):
+    def fib(self, n):
         
-#         if n==0:
-#             return 0
-#         if n==1:
-#             return 1
-#         dp=[-1]*(n+1)
-#         dp[0]=0
-#         dp[1]=1
-#         for i in range(2,n+1):
-#             dp[i]=dp[i-1]+dp[i-2]
-#         return dp[n]    
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        dp=[-1]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]    
     # def fib(self, n):
     #     if n==0:
     #         return 0
@@ -28,13 +28,13 @@ class Solution:
     #     for i in range(2,n+1):
     #         a,b=b,a+b
     #     return b  
-    def fib(self, n):
-        dp=[-1]*(n+1)
-        for i in range(n+1):
-            if i <=1:
-                dp[i]=i
-            else:
-                dp[i]=dp[i-1] +dp[i-2]
-        return dp[n]        
+    # def fib(self, n):
+    #     dp=[-1]*(n+1)
+    #     for i in range(n+1):
+    #         if i <=1:
+    #             dp[i]=i
+    #         else:
+    #             dp[i]=dp[i-1] +dp[i-2]
+    #     return dp[n]        
             
                 

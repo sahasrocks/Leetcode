66a07a34397cfa767ab959f1b84f80025f2a65b9
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2023-07-12 12:59:57 +0530

    Time: 702 ms (34.81%), Space: 23.9 MB (54.81%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,22 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n=len(graph)
+        indegree=[0]*(n)
+        adj=[[]for _ in range(n)]
+        for u in range(n):
+            for v in graph[u]:
+                adj[v].append(u)
+                indegree[u]+=1
+        queue=deque()
+        for v in range(n):
+            if indegree[v]==0:
+                queue.append(v)
+        ans=[]
+        while queue:
+            u=queue.popleft()
+            ans.append(u)
+            for v in adj[u]:
+                indegree[v]-=1
+                if indegree[v]==0:
+                    queue.append(v)
+        return sorted(ans)

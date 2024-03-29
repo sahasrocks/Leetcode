b077feb828f7a5e0183f528c1e36c779c885f522
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2024-03-29 17:04:02 +0530

    Time: 857 ms (82.44%), Space: 30.9 MB (56.87%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2962-count-subarrays-where-max-element-appears-at-least-k-times/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2962-count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        target = max(nums)
+        total = 0
+        freq = 0
+        
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] == target:
+                freq += 1
+
+            while freq >= k:
+                total += len(nums) - right  # current subarray + following
+                if nums[left] == target:
+                    freq -= 1
+                left += 1
+
+        return total
+        

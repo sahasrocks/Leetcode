3dd4aa3944b44546ab21179c22415d4ab38e6d12
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2024-10-31 14:33:38 +0530

    Time: 3 ms (81.36%), Space: 17.6 MB (81.18%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff]+1, i+1]
+            prevMap[n] = i
+             
+        

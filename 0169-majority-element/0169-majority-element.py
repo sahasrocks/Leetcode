decc582a8b288272af36d167c5e6f636cce9023d
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-09-09 20:54:59 +0530

    Time: 4 ms (81.15%), Space: 13.6 MB (65.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,15 @@
-
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        # from collections import Counter
-        # c = Counter(nums)
-        # c.most_common()
-        # value, count = c.most_common()[0]
-        # return value
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # n=0
+        # maj=None
+        # for num in nums:
+        #     if n==0:
+        #         maj= num
+        #     n +=1 if maj ==num else -1
+        # return maj        
         a=nums.sort()
         return nums[len(nums)//2]

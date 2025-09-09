ac0e08b915fda7c69c56a4f146278cf90dbcf1e9
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-09-09 21:28:54 +0530

    Time: 131 ms (24.03%), Space: 19.2 MB (45.87%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,13 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        minVal = prices[0]
-        maxProfit= 0
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        p=0
+        minb = prices[0]
         for i in prices:
-            maxProfit = max(maxProfit,i-minVal)
-            minVal = min(minVal,i)
-        return maxProfit    
+            p = max(p,i-minb)
+            minb=min(minb,i)
+        return p    
+        

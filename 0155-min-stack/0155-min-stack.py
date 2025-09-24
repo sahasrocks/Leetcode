1ac1dc43ce3f2714ce378dbd205b2b193488e0de
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-09-24 19:13:47 +0530

    Time: 3 ms (83.91%), Space: 22 MB (5.93%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,31 +1,51 @@
+# class MinStack:
+
+#     def __init__(self):
+#         self.stack=[]
+#         self.min=[]
+
+#     def push(self, val: int) -> None:
+#         self.stack.append(val)
+#         val=min(val,self.min[-1] if self.min else val)
+#         self.min.append(val)
+        
+
+#     def pop(self) -> None:
+#         self.stack.pop()
+#         self.min.pop()
+        
+
+#     def top(self) -> int:
+#         return self.stack[-1]
+        
+
+#     def getMin(self) -> int:
+#         return self.min[-1]
+
+
+# # Your MinStack object will be instantiated and called as such:
+# # obj = MinStack()
+# # obj.push(val)
+# # obj.pop()
+# # param_3 = obj.top()
+# # param_4 = obj.getMin()
 class MinStack:
 
     def __init__(self):
-        self.stack=[]
-        self.min=[]
+        self.st = []
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        val=min(val,self.min[-1] if self.min else val)
-        self.min.append(val)
+        min_val = self.getMin()
+        if min_val == None or min_val > val:
+            min_val = val
         
+        self.st.append([val, min_val])
 
     def pop(self) -> None:
-        self.stack.pop()
-        self.min.pop()
-        
+        self.st.pop()
 
     def top(self) -> int:
-        return self.stack[-1]
-        
+        return self.st[-1][0] if self.st else None
 
     def getMin(self) -> int:
-        return self.min[-1]
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+        return self.st[-1][1] if self.st else None

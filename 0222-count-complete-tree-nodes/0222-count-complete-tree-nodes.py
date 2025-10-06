806d34138046fa400355efb0a109c1d040840d1d
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-10-06 08:48:47 +0530

    Time: 5 ms (31.53%), Space: 23.3 MB (82.14%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -1,19 +1,34 @@
-class Solution(object):
-    def getHeight(self, root):
-        height = 0
-        while root:
-            height += 1
-            root = root.left
-        return height
+# class Solution(object):
+#     def getHeight(self, root):
+#         height = 0
+#         while root:
+#             height += 1
+#             root = root.left
+#         return height
 
-    def countNodes(self, root):
-        count = 0
-        while root:
-            l, r = map(self.getHeight, (root.left, root.right))
-            if l == r:
-                count += 2 ** l
-                root = root.right
-            else:
-                count += 2 ** r
-                root = root.left
-        return count
+#     def countNodes(self, root):
+#         count = 0
+#         while root:
+#             l, r = map(self.getHeight, (root.left, root.right))
+#             if l == r:
+#                 count += 2 ** l
+#                 root = root.right
+#             else:
+#                 count += 2 ** r
+#                 root = root.left
+#         return count
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        def pre(node):
+            if not node:
+                return 0
+            return pre(node.left)+pre(node.right)+1
+        return pre(root)        
+        
+        

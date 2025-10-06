4afd04f720c8cb574e013ba1af599db72e90e5d0
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-10-06 21:16:33 +0530

    Time: 7 ms (22.25%), Space: 23.3 MB (37.18%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -25,10 +25,24 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        def pre(node):
-            if not node:
-                return 0
-            return pre(node.left)+pre(node.right)+1
-        return pre(root)        
-        
+        # def pre(node):
+        #     if not node:
+        #         return 0
+        #     return pre(node.left)+pre(node.right)+1
+        # return pre(root)        
+        from collections import deque
+
+        if not root:
+            return 0
+        q=deque([root])
+        c=0
+        while q:
+            node=q.popleft()   
+            c+=1
+            if node.left:
+                q.append(node.left) 
+            if node.right:
+                q.append(node.right) 
+
+        return c           
         

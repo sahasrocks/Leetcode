43cdac34a64ea13454c7e229bd07b071c1d87c77
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-10-07 06:45:49 +0530

    Time: 4 ms (7%), Space: 18.3 MB (86.95%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,69 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root):
+        res=[]
+        q=deque([root])
+        while q:
+            n=len(q)
+            level=[]
+
+            for i in range(n):
+                node=q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level) 
+        return res               
+
+
+        
+        
+        
+        
+        
+        
+        
+        # if not root: return []
+        # q, ans = deque([root]), []
+        # while len(q):
+        #     n, curLevel = len(q), []
+        #     for i in range(n):
+        #         cur = q.popleft()
+        #         if cur.left: 
+        #             q.append(cur.left)
+        #         if cur.right: 
+        #             q.append(cur.right)
+        #         curLevel.append(cur.val)
+        #     ans.append(curLevel)
+        # return ans
+        
+        
+        # ans, level = [], [root]
+        # while root and level:
+        #     ans.append([node.val for node in level])
+        #     LRpair = [(node.left, node.right) for node in level]
+        #     level = [leaf for LR in LRpair for leaf in LR if leaf]
+        # return ans
+        # a=[]
+        # q=collections.deque()
+        # q.append(root)
+        # while q:
+        #     qlen= len(q)
+        #     level=[]
+        #     for i in range(qlen):
+        #         node=q.popleft()
+        #         if node:
+        #             level.append(node.val)
+        #             q.append(node.left)
+        #             q.append(node.right)
+        #     if level:
+        #         a.append(level)
+        # return a
+        

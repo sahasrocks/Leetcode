0654c3b94bce67721c2bc6f2ffcb698f5118adc4
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-10-15 22:17:34 +0530

    Time: 0 ms (100%), Space: 18 MB (23.49%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,11 +1,23 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
-        results = ['']
-        map = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
+        # if not digits:
+        #     return []
+        # results = ['']
+        # map = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
 
-        for digit in digits:
-            results = [result+d for result in results for d in map[digit]]
+        # for digit in digits:
+        #     results = [result+d for result in results for d in map[digit]]
 
-        return results
+        # return results
+
+        res=[]
+        digitToChar = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
+        def backtrack(i,curStr):
+            if len(curStr)==len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i+1,curStr+c)
+        if digits:
+            backtrack(0,"")
+        return res                

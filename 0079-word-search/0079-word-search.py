40c8af9d3ca85a6844cc93cd7dfcf8158c39eca4
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-10-28 17:07:27 +0530

    Time: 4393 ms (47.01%), Space: 17.7 MB (87.8%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,18 +1,46 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def search(board, x, y, word, visited=set()):
-            if word == '':
+        rows=len(board)
+        cols=len(board[0])
+        path=set()
+
+        def dfs(r,c,i):
+            if i==len(word):
                 return True
-            for i, j in [[x + 1, y], [x - 1, y], [x, y - 1], [x, y + 1]]:
-                if 0 <= i < len(board) and 0 <= j < len(board[0]) and (i, j) not in visited and board[i][j] == word[0] \
-                and search(board, i, j, word[1:], visited | {(x, y)}):
+            if ( r<0 or c<0 or r>=rows or c>=cols or word[i] != board[r][c] or (r,c) in path ):
+                return False
+            path.add((r,c))
+            res = dfs(r+1,c,i+1) or dfs(r,c+1,i+1) or dfs(r-1,c,i+1) or dfs(r,c-1,i+1)
+            path.remove((r,c))
+            return res
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r,c,0):
                     return True
-            return False
+        return False                
+
         
-        if not board or not board[0]:
-            return False
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0] and search(board, i, j, word[1:]):
-                    return True
-        return False
+        
+        
+        
+        
+        
+        
+        
+        
+        # def search(board, x, y, word, visited=set()):
+        #     if word == '':
+        #         return True
+        #     for i, j in [[x + 1, y], [x - 1, y], [x, y - 1], [x, y + 1]]:
+        #         if 0 <= i < len(board) and 0 <= j < len(board[0]) and (i, j) not in visited and board[i][j] == word[0] \
+        #         and search(board, i, j, word[1:], visited | {(x, y)}):
+        #             return True
+        #     return False
+        
+        # if not board or not board[0]:
+        #     return False
+        # for i in range(len(board)):
+        #     for j in range(len(board[0])):
+        #         if board[i][j] == word[0] and search(board, i, j, word[1:]):
+        #             return True
+        # return False

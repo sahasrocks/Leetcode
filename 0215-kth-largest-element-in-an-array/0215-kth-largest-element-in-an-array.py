ee46178604f1b51b078c17f2c3c15907adfd1fe5
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-10-30 16:32:23 +0530

    Time: 61 ms (73.26%), Space: 28.7 MB (45.06%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,13 +1,26 @@
 class Solution:
     def findKthLargest(self, nums, k):
-        pivot = nums[0]
-        left  = [l for l in nums if l < pivot]
-        equal = [e for e in nums if e == pivot]
-        right = [r for r in nums if r > pivot]
+        
+        min_heap=[]
+        for num in nums:
+            if len(min_heap)<k:
+                heapq.heappush(min_heap,num)
+            else:
+                heapq.heappushpop(min_heap,num)
+        return min_heap[0]            
+        
+        
+        
+        
+        
+        # pivot = nums[0]
+        # left  = [l for l in nums if l < pivot]
+        # equal = [e for e in nums if e == pivot]
+        # right = [r for r in nums if r > pivot]
 
-        if k <= len(right):
-            return self.findKthLargest(right, k)
-        elif (k - len(right)) <= len(equal):
-            return equal[0]
-        else:
-            return self.findKthLargest(left, k - len(right) - len(equal))    
+        # if k <= len(right):
+        #     return self.findKthLargest(right, k)
+        # elif (k - len(right)) <= len(equal):
+        #     return equal[0]
+        # else:
+        #     return self.findKthLargest(left, k - len(right) - len(equal))    

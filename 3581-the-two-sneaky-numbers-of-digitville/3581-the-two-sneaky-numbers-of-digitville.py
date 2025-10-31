a1fe0bb87a052b7baf9cdfa2b26a025b974fc6b4
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-10-31 06:05:20 +0530

    Time: 0 ms (100%), Space: 17.7 MB (66.29%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -1,6 +1,14 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        c=Counter(nums)
-        return [num for num,_ in c.most_common(2)]
+        # c=Counter(nums)
+        # return [num for num,_ in c.most_common(2)]
+        visited=set()
+        res=[]
+        for num in nums:
+            if num in visited:
+                res.append(num)
+            else:
+                visited.add(num)
+        return res            
 
         

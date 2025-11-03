e80ee2b3c588125d542a1f6bf9931cfe6ba234ad
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-11-03 07:07:43 +0530

    Time: 0 ms (100%), Space: 18.4 MB (89.92%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -4,7 +4,25 @@ class Solution:
         #     diff= target - v
         #     if diff in numbers:
         #         return (i+1, numbers.index(diff)+1)
-        l,r=0,len(numbers)-1
+        # l,r=0,len(numbers)-1
+        # while l<r:
+        #     s=numbers[l]+numbers[r]
+        #     if s==target:
+        #         return [l+1,r+1]
+        #     elif s<target:
+        #         l+=1
+        #     else:
+        #         r-=1
+
+
+
+
+
+
+
+
+        l=0
+        r=len(numbers)-1
         while l<r:
             s=numbers[l]+numbers[r]
             if s==target:
@@ -12,6 +30,5 @@ class Solution:
             elif s<target:
                 l+=1
             else:
-                r-=1
-
+                r-=1        
 

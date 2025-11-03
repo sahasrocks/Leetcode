30dc33612d40f2f72d51caf36609d69d56d77f1a
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-11-03 09:11:08 +0530

    Time: 10 ms (73.8%), Space: 19.4 MB (39.04%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,16 +1,38 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        l,r=0,len(height)-1
+        leftMax,rightMax=height[l],height[r]
+        res=0
+        while l<r:
+            if leftMax<rightMax:
+                l+=1
+                leftMax=max(leftMax,height[l])
+                res+=leftMax-height[l]
+            else:
+                r-=1
+                rightMax=max(rightMax,height[r])
+                res+=rightMax-height[r]
+        return res                
         
-        i, j, ans, mx, mi = 0, len(height) - 1, 0, 0, 0
-        while i <= j:
-            # take the min height
-            mi = min(height[i], height[j])
-            # find the max min height
-            mx = max(mx, mi)
-            # the units of water being tapped is the diffence between max height and min height
-            ans += mx - mi
-            # move the pointer i if height[i] is smaller
-            if height[i] < height[j]: i += 1
-            # else move pointer j
-            else: j -= 1
-        return ans
+        
+        
+        
+        
+        
+        
+        
+        # i, j, ans, mx, mi = 0, len(height) - 1, 0, 0, 0
+        # while i <= j:
+        #     # take the min height
+        #     mi = min(height[i], height[j])
+        #     # find the max min height
+        #     mx = max(mx, mi)
+        #     # the units of water being tapped is the diffence between max height and min height
+        #     ans += mx - mi
+        #     # move the pointer i if height[i] is smaller
+        #     if height[i] < height[j]: i += 1
+        #     # else move pointer j
+        #     else: j -= 1
+        # return ans

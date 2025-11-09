8f79c2fc3f3a24f9d1cdfc90706dcad8cdb28a9e
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-11-09 15:29:05 +0530

    Time: 0 ms (100%), Space: 17.7 MB (87.36%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,14 +1,47 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp=[False]*(len(s)+1)
 
-        dp = [False] * (len(s) + 1)
-        dp[len(s)] = True
+        dp[len(s)]=True
 
-        for i in range(len(s) - 1, -1, -1):
+        for i in range(len(s)-1,-1,-1):
             for w in wordDict:
-                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
-                    dp[i] = dp[i + len(w)]
+                if (i+len(w)) <=len(s) and s[i:i+len(w)]==w:
+                    dp[i]=dp[i+len(w)]
                 if dp[i]:
                     break
+        return dp[0]                
 
-        return dp[0]
+
+
+
+        # wordset=set(wordDict)
+        # def dfs(i):
+        #     if i==len(s):
+        #         return True
+        #     for j in range(len(s)):
+        #         if s[i:j+1] in wordset:
+        #             if dfs(j+1):
+        #                 return True
+        #     return False
+        # return dfs(0)                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # dp = [False] * (len(s) + 1)
+        # dp[len(s)] = True
+
+        # for i in range(len(s) - 1, -1, -1):
+        #     for w in wordDict:
+        #         if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+        #             dp[i] = dp[i + len(w)]
+        #         if dp[i]:
+        #             break
+
+        # return dp[0]

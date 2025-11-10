959c4faef9bf6a214acd218f0ca6c58b3aefc5b4
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-11-10 08:41:40 +0530

    Time: 35 ms (21.69%), Space: 21.4 MB (16.88%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,24 +1,50 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        
+        m, n = len(grid), len(grid[0])
+        dp = [[-1] * n for _ in range(m)]
+
+        def dfs(r, c):
+            if r == m - 1 and c == n - 1:
+                return grid[r][c]
+            if r == m or c == n:
+                return float('inf')
+            if dp[r][c] != -1:
+                return dp[r][c]
+
+            dp[r][c] = grid[r][c] + min(dfs(r + 1, c), dfs(r, c + 1))
+            return dp[r][c]
+
+        return dfs(0, 0)                
+
+        # def dfs(r, c):
+        #     if r == len(grid) - 1 and c == len(grid[0]) - 1:
+        #         return grid[r][c]
+        #     if r == len(grid) or c == len(grid[0]):
+        #         return float('inf')
+        #     return grid[r][c] + min(dfs(r + 1, c), dfs(r, c + 1))
+
+        # return dfs(0, 0)            
+
+
+
         # get dimensions
-        n = len(grid) # no of cells in each col
-        m = len(grid[0]) # no of cells in each row
+        # n = len(grid) # no of cells in each col
+        # m = len(grid[0]) # no of cells in each row
         
-        # populate first row using m for no of cells in row
-        for i in range(1,m):
-            grid[0][i] = grid[0][i] + grid[0][i-1]
+        # # populate first row using m for no of cells in row
+        # for i in range(1,m):
+        #     grid[0][i] = grid[0][i] + grid[0][i-1]
         
-        # populate first col using n for no of cells in col
-        for j in range(1,n):
-            grid[j][0] = grid[j-1][0] + grid[j][0]
+        # # populate first col using n for no of cells in col
+        # for j in range(1,n):
+        #     grid[j][0] = grid[j-1][0] + grid[j][0]
         
-        # populate the rest
-        for i in range(1,n):
-            for j in range(1,m):
-				# get min seen so far plus curr cell value
-                grid[i][j] = min(grid[i-1][j],grid[i][j-1]) + grid[i][j]
+        # # populate the rest
+        # for i in range(1,n):
+        #     for j in range(1,m):
+		# 		# get min seen so far plus curr cell value
+        #         grid[i][j] = min(grid[i-1][j],grid[i][j-1]) + grid[i][j]
         
-        # return last cell
-        return grid[-1][-1]
+        # # return last cell
+        # return grid[-1][-1]
         

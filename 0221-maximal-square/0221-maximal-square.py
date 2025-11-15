df06ad5d90e8cd1dea76dcafc0609ec15975627b
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-11-15 22:24:46 +0530

    Time: 112 ms (90.3%), Space: 33.1 MB (57.75%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,19 +1,13 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        ROWS, COLS = len(matrix), len(matrix[0])
-        cache = {}
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        max_square = 0
 
-        def dfs(r, c):
-            if r >= ROWS or c >= COLS:
-                return 0
-            if (r, c) not in cache:
-                down = dfs(r + 1, c)
-                right = dfs(r, c + 1)
-                diag = dfs(r + 1, c + 1)
-                cache[(r, c)] = 0
+        for r in range(m - 1, -1, -1):
+            for c in range(n - 1, -1, -1):
                 if matrix[r][c] == "1":
-                    cache[(r, c)] = 1 + min(down, right, diag)
-            return cache[(r, c)]
+                    dp[r][c] = 1 + min(dp[r + 1][c], dp[r][c + 1], dp[r + 1][c + 1])
+                    max_square = max(max_square, dp[r][c])
 
-        dfs(0, 0)
-        return max(cache.values()) ** 2
+        return max_square * max_square

e69596a6a9542f8770deed0ad86647e964f61221
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-11-16 17:49:17 +0530

    Time: 15 ms (84.73%), Space: 17.7 MB (97.79%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,14 +1,62 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
 
-        n1, n2 = len(s1), len(s2)                   # Example: s1 = "abi"  s2 = "eidbiaoo"
-                                                    # ctr1 = {b:2, a:1, i:1}
-        ctr1, ctr2 = Counter(s1), Counter(s2[:n1])  
-                                                    #  i     ctr2                  ctr1 == ctr2 
-        for i in range(n1,n2):                      # ––––   –––––––––––––––       ––––––––––––
-            if ctr1 == ctr2: return True            #  4     {e:1, i:1, d:1, b:1}      False
-                                                    #  5     {b:2, i:1, d:1}           False
-            ctr2[s2[i-n1]]-= 1                      #  6     {b:2, a:1, i:1}           True
-            ctr2[s2[i   ]]+= 1
+        s1Count = [0] * 26
+        s2Count = [0] * 26
 
-        return ctr1 == ctr2
+        for c in s1:
+            s1Count[ord(c) - ord('a')] += 1
+
+        l = 0
+        for r in range(len(s2)):
+            s2Count[ord(s2[r]) - ord('a')] += 1
+
+            if (r - l + 1) > len(s1):
+                s2Count[ord(s2[l]) - ord('a')] -= 1
+                l += 1
+
+            if s1Count == s2Count:
+                return True
+
+        return False
+
+# class Solution:
+#     def checkInclusion(self, s1: str, s2: str) -> bool:
+#         s1 = sorted(s1)
+
+#         for i in range(len(s2)):
+#             for j in range(i, len(s2)):
+#                 subStr = s2[i : j + 1]
+#                 subStr = sorted(subStr)
+#                 if subStr == s1:
+#                     return True
+#         return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # n1, n2 = len(s1), len(s2)                   # Example: s1 = "abi"  s2 = "eidbiaoo"
+        #                                             # ctr1 = {b:2, a:1, i:1}
+        # ctr1, ctr2 = Counter(s1), Counter(s2[:n1])  
+        #                                             #  i     ctr2                  ctr1 == ctr2 
+        # for i in range(n1,n2):                      # ––––   –––––––––––––––       ––––––––––––
+        #     if ctr1 == ctr2: return True            #  4     {e:1, i:1, d:1, b:1}      False
+        #                                             #  5     {b:2, i:1, d:1}           False
+        #     ctr2[s2[i-n1]]-= 1                      #  6     {b:2, a:1, i:1}           True
+        #     ctr2[s2[i   ]]+= 1
+
+        # return ctr1 == ctr2

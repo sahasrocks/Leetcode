526957b808de6d7d9733f4a7da6f92d3e9b3bb32
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-11-16 18:07:35 +0530

    Time: 0 ms (100%), Space: 18 MB (36.47%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -11,6 +11,22 @@ class Solution:
             else:
                 stack.append(c)
         return True if not stack else False                    
+        
+        
+        
+        # stack=[]
+        # closetoopen={")":"(","]":"[","}":"{"}
+        # for c in s:
+        #     if c in closetoopen:
+        #         print(closetoopen[c])
+        #         if stack and stack[-1]==closetoopen[c]:
+                    
+        #             stack.pop()
+        #         else:
+        #             return False
+        #     else:
+        #         stack.append(c)
+        # return True if not stack else False                    
 
 
         

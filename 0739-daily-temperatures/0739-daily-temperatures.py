aba825a56f4023c76243a941efc00592db555059
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-11-17 06:31:55 +0530

    Time: 134 ms (21.56%), Space: 35.6 MB (5.46%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,10 +1,40 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        l = len(temperatures)
-        result, stack = [0]*l, deque()
-        for i in range(l):
-            while stack and temperatures[stack[-1]] < temperatures[i]:
-                index = stack.pop()
-                result[index] = i-index
-            stack.append(i)
-        return result
+        res=[0] * len(temperatures)
+        stack=[]
+        for i,t in enumerate(temperatures):
+            while stack and t>stack[-1][0]:
+                stackT,stackInd=stack.pop()
+                res[stackInd]=i-stackInd
+            stack.append([t,i])
+        return res        
+        
+        
+        # res = []
+        # n = len(temperatures)
+        # for i in range(n):
+        #     days_to_wait = 0
+        #     for j in range(i + 1, n): # Start searching from the next day (i + 1)
+        #         days_to_wait += 1
+        #         if temperatures[j] > temperatures[i]:
+        #             res.append(days_to_wait)
+        #             break
+        #     else: # This 'else' executes if the 'for j' loop completes without a 'break'
+        #         res.append(0) # No warmer day found, so append 0
+        # return res          
+        
+        
+        
+        
+        
+        
+        
+        
+        # l = len(temperatures)
+        # result, stack = [0]*l, deque()
+        # for i in range(l):
+        #     while stack and temperatures[stack[-1]] < temperatures[i]:
+        #         index = stack.pop()
+        #         result[index] = i-index
+        #     stack.append(i)
+        # return result

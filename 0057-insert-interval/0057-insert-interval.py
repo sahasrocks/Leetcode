2ef1aaf5a108c76a6c9200f802d43b6a32fd7dab
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-11-18 22:03:22 +0530

    Time: 0 ms (100%), Space: 19.9 MB (18.69%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,16 +1,30 @@
 class Solution:
     def insert(self, intervals, newInterval):
+        intervals.append(newInterval)
         res=[]
-        for i in range(len(intervals)):
-            if newInterval[1]<intervals[i][0]:
-                res.append(newInterval)
-                return  res + intervals[i:]
-            elif newInterval[0]>intervals[i][1]: 
-                res.append(intervals[i])
+        for i in sorted(intervals, key=lambda x:x[0]):
+            if res and res[-1][1]>=i[0]:
+                res[-1][1]=max(i[1],res[-1][1])
             else:
-                newInterval=[min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
-        res.append(newInterval)   
-        return res         
+                res.append(i)
+        return res  
+        
+        
+        
+        
+        
+        
+        # res=[]
+        # for i in range(len(intervals)):
+        #     if newInterval[1]<intervals[i][0]:
+        #         res.append(newInterval)
+        #         return  res + intervals[i:]
+        #     elif newInterval[0]>intervals[i][1]: 
+        #         res.append(intervals[i])
+        #     else:
+        #         newInterval=[min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
+        # res.append(newInterval)   
+        # return res         
                 
 
 

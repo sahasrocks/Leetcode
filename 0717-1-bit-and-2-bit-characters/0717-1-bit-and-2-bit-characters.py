f3fffbc2f35c55cac1bee5014ff0370ba5c20d88
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-11-18 15:44:43 +0530

    Time: 0 ms (100%), Space: 17.7 MB (91.02%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
@@ -1,12 +1,26 @@
 class Solution:
     def isOneBitCharacter(self, bits):
-        i, n, numBits = 0, len(bits), 0
-        while i < n:
-            bit = bits[i]
-            if bit == 1:
-                i += 2
-                numBits = 2
-            else:
-                i += 1
-                numBits = 1
-        return numBits == 1
+        q=collections.deque(bits)
+
+        last=False
+        while len(q)>0:
+            a=q.popleft()
+            if a==0:
+                last=True
+                continue
+            last=False
+            q.popleft()
+        return last        
+
+        
+        
+        # i, n, numBits = 0, len(bits), 0
+        # while i < n:
+        #     bit = bits[i]
+        #     if bit == 1:
+        #         i += 2
+        #         numBits = 2
+        #     else:
+        #         i += 1
+        #         numBits = 1
+        # return numBits == 1

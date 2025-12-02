7fdff598e81f2947d438c9d0a805854950ef88b0
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-12-02 06:01:02 +0530

    Time: 43 ms (81.28%), Space: 34.5 MB (44.68%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,14 +1,40 @@
 class Solution(object):
     def partition(self, s):
         res=[]
-        
-        def helper(arr, str):
-            if str:
-                for i in range(1, len(str)+1):
-                    if str[:i] == str[:i][::-1]:
-                        helper(arr+[str[:i]], str[i:])
-            elif arr: 
-                    res.append(arr)
-        
-        helper([], s)
+        part=[]
+        def dfs(i):
+            if i>=len(s):
+                res.append(part.copy())
+                return
+            for j in range(i,len(s)):
+                if self.ispali(s,i,j):
+                    part.append(s[i:j+1])
+                    dfs(j+1)
+                    part.pop()
+        dfs(0)
         return res
+
+    def ispali(self,s,l,r):
+        while l<r:
+            if s[l] != s[r]:
+                return False
+            l,r=l+1,r-1
+        return True                            
+        
+        
+        
+        
+        # res=[]
+        # def pallindrome(a):
+        #     return a==a[::-1]
+        # def dfs(i,cur):
+        #     if i==len(s):
+        #         res.append(cur)
+        #         return
+        #     for j in range(i,len(s)):
+        #         sol=s[i:j+1]
+        #         if pallindrome(sol):
+        #             dfs(j+1,cur+[sol])
+        #     return
+        # dfs(0,[])
+        # return res                    

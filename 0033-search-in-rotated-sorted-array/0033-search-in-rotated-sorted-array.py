a63e2ab3243bc764f9997792afbd3df96aa6a1b9
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-12-02 07:18:11 +0530

    Time: 0 ms (100%), Space: 18.1 MB (66.19%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -2,21 +2,41 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l,r=0,len(nums)-1
         while l<=r:
-            mid=(l+r)//2
-            if nums[mid]==target:
-                return mid
-            #left sorted arr
-            if nums[l]<=nums[mid]:
-                if target >nums[mid] or target<nums[l]:
-                    l=mid+1
+            m=(l+r)//2
+            if nums[m]==target:
+                return m
+            if nums[l]<=nums[m]:
+                if target>nums[m] or target<nums[l]:
+                    l=m+1
                 else:
-                    r=mid-1
+                    r=m-1
             else:
-                if target<nums[mid] or target>nums[r]:
-                    r=mid-1
+                if target < nums[m] or target> nums[r]:
+                    r=m-1
                 else:
-                    l=mid+1
+                    l=m+1
         return -1                                
+        
+        
+        
+        
+        # l,r=0,len(nums)-1
+        # while l<=r:
+        #     mid=(l+r)//2
+        #     if nums[mid]==target:
+        #         return mid
+        #     #left sorted arr
+        #     if nums[l]<=nums[mid]:
+        #         if target >nums[mid] or target<nums[l]:
+        #             l=mid+1
+        #         else:
+        #             r=mid-1
+        #     else:
+        #         if target<nums[mid] or target>nums[r]:
+        #             r=mid-1
+        #         else:
+        #             l=mid+1
+        # return -1                                
         
         # for i in range(len(nums)):
         #     if nums[i]==target:

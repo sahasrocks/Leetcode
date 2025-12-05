71c7df1321abc3631196d6d06e5d3dbbb28bd870
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-12-05 08:52:30 +0530

    Time: 43 ms (76.05%), Space: 19.6 MB (53.23%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -6,19 +6,34 @@
 
 class Solution(object):
     def detectCycle(self, head):
+        slow,fast=head,head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+            if slow==fast:
+                break
+        else:
+            return None
+        while head != slow:
+            head,slow=head.next,slow.next
+        return head                
+
+
+       
+       
         # Initialize pointers at head of linkedlist...
-        p1 = p2 = head
-        # Run a loop until p2 and p2.next is equal to null...
-        while p2 and p2.next:
-            # Moving p1 by 1 & p2 by 2
-            p1, p2 = p1.next, p2.next.next
-            # found the cycle...
-            if p1 == p2: break
-        # In case there is no cycle or no meeting point...
-        else: return None
-        # run loop until again head & p1 don't collab...
-        while head != p1:
-            # Moving head by 1 & p1 by 1 as well...
-            head, p1 = head.next, p1.next
-        return head 
+        # p1 = p2 = head
+        # # Run a loop until p2 and p2.next is equal to null...
+        # while p2 and p2.next:
+        #     # Moving p1 by 1 & p2 by 2
+        #     p1, p2 = p1.next, p2.next.next
+        #     # found the cycle...
+        #     if p1 == p2: break
+        # # In case there is no cycle or no meeting point...
+        # else: return None
+        # # run loop until again head & p1 don't collab...
+        # while head != p1:
+        #     # Moving head by 1 & p1 by 1 as well...
+        #     head, p1 = head.next, p1.next
+        # return head 
         

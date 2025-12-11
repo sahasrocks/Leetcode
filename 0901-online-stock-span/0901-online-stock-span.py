e06cc226f2830aba9e9c248b72dacd4b04ae8bcb
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-12-11 07:39:28 +0530

    Time: 56 ms (74.26%), Space: 22.1 MB (90.64%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.py
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -1,22 +1,42 @@
 class StockSpanner:
 
     def __init__(self):
-        self.st=[]
-        self.lst=[]
+        self.stack=[]
         
 
     def next(self, price: int) -> int:
-        if self.st==[]:
-            self.st.append(price)
-            self.lst.append(1)
-            return 1
-        ct=1
-        while self.st:
-            if self.st[-1]<=price:
-                ct+=self.lst.pop()
-                self.st.pop()
-            else:
-                break
-        self.st.append(price)
-        self.lst.append(ct)
-        return ct
+        span=1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack[-1][1]
+            self.stack.pop()
+        self.stack.append([price,span])
+        return span    
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
+
+#class StockSpanner:
+
+#     def __init__(self):
+#         self.st=[]
+#         self.lst=[]
+        
+
+#     def next(self, price: int) -> int:
+#         if self.st==[]:
+#             self.st.append(price)
+#             self.lst.append(1)
+#             return 1
+#         ct=1
+#         while self.st:
+#             if self.st[-1]<=price:
+#                 ct+=self.lst.pop()
+#                 self.st.pop()
+#             else:
+#                 break
+#         self.st.append(price)
+#         self.lst.append(ct)
+#         return ct

943c011df259b46ad758c0b04f7ae00800a7e793
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-12-15 05:47:36 +0530

    Time: 187 ms (35.7%), Space: 31.9 MB (42.16%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,18 +1,34 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        from collections import deque
-        q = deque() ;res = []
-        j = 0 ; i = 0
-        while j<len(nums):
-            while q and nums[j]>q[-1]:
+        q=collections.deque()
+        output=[]
+        l=r=0
+        while r<len(nums):
+            while q and nums[q[-1]]<nums[r]:
                 q.pop()
-            q.append(nums[j])
-            if j-i+1<k:
-                j+=1
-            elif j-i+1==k:
-                res.append(q[0])
-                if q[0]==nums[i]:
-                    q.popleft()
-                i+=1
-                j+=1
-        return res
+            q.append(r)
+            if l>q[0]:
+                q.popleft()
+            if r+1 >=k:
+                output.append(nums[q[0]])
+                l+=1
+            r+=1
+        return output                
+        
+        
+        # from collections import deque
+        # q = deque() ;res = []
+        # j = 0 ; i = 0
+        # while j<len(nums):
+        #     while q and nums[j]>q[-1]:
+        #         q.pop()
+        #     q.append(nums[j])
+        #     if j-i+1<k:
+        #         j+=1
+        #     elif j-i+1==k:
+        #         res.append(q[0])
+        #         if q[0]==nums[i]:
+        #             q.popleft()
+        #         i+=1
+        #         j+=1
+        # return res

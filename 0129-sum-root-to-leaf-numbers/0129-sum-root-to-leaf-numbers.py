0029a62ae63bd3f548f7067d4e6ed5ded2447937
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-12-27 16:18:44 +0530

    Time: 0 ms (100%), Space: 17 MB (99.85%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -10,6 +10,16 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        def dfs(root,sum):
+            if not root:
+                return 0
+            sum= sum*10 + root.val
+            if not root.left and not root.right:
+                return sum
+            return dfs(root.left,sum)+ dfs(root.right,sum)
+        return dfs(root,0)            
+
+
     #     return self.dfs(root, 0)
     
     # def dfs(self, root, sum):
@@ -21,16 +31,16 @@ class Solution(object):
     #     return self.dfs(root.left, sum) + self.dfs(root.right, sum)
         
 
-        if not root:
-            return 0
-        total=0
-        stack=[(root,root.val)]
-        while stack:
-            node,cur_num=stack.pop()
-            if not node.left and not node.right:
-                total+=cur_num
-            if node.right:
-                stack.append((node.right,cur_num*10 +node.right.val))     
-            if node.left:
-                stack.append((node.left,cur_num*10 +node.left.val))
-        return total        
+        # if not root:
+        #     return 0
+        # total=0
+        # stack=[(root,root.val)]
+        # while stack:
+        #     node,cur_num=stack.pop()
+        #     if not node.left and not node.right:
+        #         total+=cur_num
+        #     if node.right:
+        #         stack.append((node.right,cur_num*10 +node.right.val))     
+        #     if node.left:
+        #         stack.append((node.left,cur_num*10 +node.left.val))
+        # return total        

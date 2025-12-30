253abe94dd4dbde9412332809f2d385592eeec39
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2025-12-30 12:03:12 +0530

    Time: 3 ms (76.07%), Space: 18.9 MB (84.99%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,25 +1,51 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        prereq={c:[] for c in range(numCourses)}
+        preMap=defaultdict(list)
         for cre,pre in prerequisites:
-            prereq[cre].append(pre)
+            preMap[cre].append(pre)
         cycle=set()
-        visit=set()
+        visited=set()
         output=[]
-        def dfs(cre):
-            if cre in cycle:
+        def dfs(crs):
+            if crs in cycle:
                 return False
-            if cre in visit:
+            if crs in visited:
                 return True
-            cycle.add(cre)
-            for pre in prereq[cre]:
+            cycle.add(crs)
+            for pre in preMap[crs]:
                 if dfs(pre) == False:
                     return False
-            visit.add(cre)                    
-            cycle.remove(cre)
-            output.append(cre)
+            visited.add(crs)
+            cycle.remove(crs)
+            output.append(crs)
             return True
         for c in range(numCourses):
             if dfs(c) == False:
                 return []
-        return output            
+        return output                         
+
+
+        
+        # prereq={c:[] for c in range(numCourses)}
+        # for cre,pre in prerequisites:
+        #     prereq[cre].append(pre)
+        # cycle=set()
+        # visit=set()
+        # output=[]
+        # def dfs(cre):
+        #     if cre in cycle:
+        #         return False
+        #     if cre in visit:
+        #         return True
+        #     cycle.add(cre)
+        #     for pre in prereq[cre]:
+        #         if dfs(pre) == False:
+        #             return False
+        #     visit.add(cre)                    
+        #     cycle.remove(cre)
+        #     output.append(cre)
+        #     return True
+        # for c in range(numCourses):
+        #     if dfs(c) == False:
+        #         return []
+        # return output            

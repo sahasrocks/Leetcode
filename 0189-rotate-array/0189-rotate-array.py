1221749470c92d16a99a7ab18842d24eaf25ba38
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-05 16:59:39 +0530

    Time: 3 ms (75.15%), Space: 24.3 MB (89.19%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,7 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
-        nums[:] = nums[-k:] + nums[:-k]    
+        # k = k % len(nums)
+        # nums[:] = nums[-k:] + nums[:-k] 
+        def swap(arr,i,j):
+            while i<j:
+                arr[i],arr[j]=arr[j],arr[i]
+                i+=1
+                j-=1
+            return arr
+        if k>len(nums):
+            k=k%len(nums)
+        if k>0:
+            swap(nums,0,len(nums)-1)
+            swap(nums,0,k-1)
+            swap(nums,k,len(nums)-1)               
               
         

9501a4e43d969aed0be4d9548090f0fe76e04c46
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-06 18:50:18 +0530

    Time: 50 ms (63.3%), Space: 39.7 MB (7.84%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,15 +1,25 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        dic = {0:-1}
-        summ = 0
-        for i, n in enumerate(nums):
-            if k != 0:
-                summ = (summ + n) % k
-            else:
-                summ += n
-            if summ not in dic:
-                dic[summ] = i
-            else:
-                if i - dic[summ] >= 2:
-                    return True
-        return False
+        m={0:-1}
+        total=0
+        for i,n in enumerate(nums):
+            total+=n
+            r=total%k
+            if r not in m:
+                m[r]=i
+            elif i-m[r]>1:
+                return True
+        return False            
+        # dic = {0:-1}
+        # summ = 0
+        # for i, n in enumerate(nums):
+        #     if k != 0:
+        #         summ = (summ + n) % k
+        #     else:
+        #         summ += n
+        #     if summ not in dic:
+        #         dic[summ] = i
+        #     else:
+        #         if i - dic[summ] >= 2:
+        #             return True
+        # return False

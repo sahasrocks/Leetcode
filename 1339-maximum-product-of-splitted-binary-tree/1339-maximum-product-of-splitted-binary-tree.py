92ea7b624891dfdad032bf451bcdb1c67e2015a4
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-07 08:00:49 +0530

    Time: 64 ms (60.08%), Space: 46.6 MB (5.32%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
@@ -6,13 +6,31 @@
 #         self.right = right
 class Solution:
     def maxProduct(self, root: Optional[TreeNode]) -> int:
-        def dfs(root):
-            if root == None: return 0
-            currSum = dfs(root.left) + dfs(root.right) + root.val
-            self.ans = max(self.ans, currSum * (self.totalSum - currSum))
-            return currSum
+        def total(root):
+            if not root:
+                return 0
+            return root.val + total(root.left) + total(root.right)
+        mod = 10**9 +7
+        treeSum=total(root)
+        best=0
+        def findMax(node):
+            if not node:
+                return 0
+            subTreeSum = node.val + findMax(node.left) + findMax(node.right)
+            nonlocal best
+            best = max(best, subTreeSum * (treeSum-subTreeSum))
+            return subTreeSum
+        findMax(root)
+        return best % mod        
 
-        self.ans, self.totalSum = 0, 0
-        self.totalSum = dfs(root)  # Firstly, get total sum of all nodes in the Binary Tree
-        dfs(root)  # Then dfs in post order to calculate sum of each subtree and its complement
-        return self.ans % (10 ** 9 + 7)
+        
+        # def dfs(root):
+        #     if root == None: return 0
+        #     currSum = dfs(root.left) + dfs(root.right) + root.val
+        #     self.ans = max(self.ans, currSum * (self.totalSum - currSum))
+        #     return currSum
+
+        # self.ans, self.totalSum = 0, 0
+        # self.totalSum = dfs(root)  # Firstly, get total sum of all nodes in the Binary Tree
+        # dfs(root)  # Then dfs in post order to calculate sum of each subtree and its complement
+        # return self.ans % (10 ** 9 + 7)

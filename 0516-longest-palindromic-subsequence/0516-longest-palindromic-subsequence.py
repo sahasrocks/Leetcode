34927a24d9dc20ee3dc827b206769f0442016307
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-08 08:51:08 +0530

    Time: 775 ms (68.69%), Space: 257.4 MB (6.68%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,11 +1,27 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        rev_s = s[::-1]
-        dp = [[0 for _ in range(len(s)+1)] for _ in range(len(s)+1)]
-        for i in range(1, len(s)+1):
-            for j in range(1, len(s)+1):
-                if s[i-1] == rev_s[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
-                else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        return dp[-1][-1]
+        memo={}
+        def dfs(i,j):
+            if i>j:
+                return 0
+            if i==j:
+                return 1
+            if (i,j) in memo:
+                return memo[(i,j)]
+            if s[i]==s[j]:
+                memo[(i,j)] = dfs(i+1,j-1)+2
+            else:
+                memo[(i,j)] = max(dfs(i+1,j),dfs(i,j-1))
+            return memo[(i,j)]
+        return dfs(0,len(s)-1)                        
+        
+        
+        # rev_s = s[::-1]
+        # dp = [[0 for _ in range(len(s)+1)] for _ in range(len(s)+1)]
+        # for i in range(1, len(s)+1):
+        #     for j in range(1, len(s)+1):
+        #         if s[i-1] == rev_s[j-1]:
+        #             dp[i][j] = 1 + dp[i-1][j-1]
+        #         else:
+        #             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        # return dp[-1][-1]

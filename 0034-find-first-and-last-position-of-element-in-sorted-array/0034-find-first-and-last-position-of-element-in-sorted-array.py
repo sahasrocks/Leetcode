8507d11ad63474018f2f5d7eb5dc01530170212f
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-09 19:59:23 +0530

    Time: 0 ms (100%), Space: 20.8 MB (5.15%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -4,14 +4,14 @@ class Solution:
         right=self.bs(nums,target,False)
         return [left,right]
     
-    def bs(self,nums,target,leftbias):
+    def bs(self,nums,t,leftbias):
         l,r=0,len(nums)-1
         i=-1
         while l<=r:
             m=(l+r)//2
-            if target>nums[m]:
+            if t>nums[m]:
                 l=m+1
-            elif target<nums[m]:
+            elif t<nums[m]:
                 r=m-1
             else:
                 i=m
@@ -20,6 +20,23 @@ class Solution:
                 else:
                     l=m+1
         return i                        
+    
+    # def bs(self,nums,target,leftbias):
+    #     l,r=0,len(nums)-1
+    #     i=-1
+    #     while l<=r:
+    #         m=(l+r)//2
+    #         if target>nums[m]:
+    #             l=m+1
+    #         elif target<nums[m]:
+    #             r=m-1
+    #         else:
+    #             i=m
+    #             if leftbias:
+    #                 r=m-1
+    #             else:
+    #                 l=m+1
+    #     return i                        
     
     
     

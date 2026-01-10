202ae8fef8a8d31cdbd0e73212d7e31a570b848a
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-10 18:37:50 +0530

    Time: 48 ms (56.3%), Space: 33.8 MB (70.01%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,25 +1,47 @@
 class Solution(object):
+    def ispali(self,s,l,r):
+        while l<=r:
+            if s[l] !=s[r]:
+                return False
+            l+=1
+            r-=1
+        return True        
     def partition(self, s):
         res=[]
         part=[]
         def dfs(i):
             if i>=len(s):
                 res.append(part.copy())
-                return
+                return 
             for j in range(i,len(s)):
                 if self.ispali(s,i,j):
                     part.append(s[i:j+1])
                     dfs(j+1)
                     part.pop()
         dfs(0)
-        return res
+        return res                    
+        
+        
+        # res=[]
+        # part=[]
+        # def dfs(i):
+        #     if i>=len(s):
+        #         res.append(part.copy())
+        #         return
+        #     for j in range(i,len(s)):
+        #         if self.ispali(s,i,j):
+        #             part.append(s[i:j+1])
+        #             dfs(j+1)
+        #             part.pop()
+        # dfs(0)
+        # return res
 
-    def ispali(self,s,l,r):
-        while l<r:
-            if s[l] != s[r]:
-                return False
-            l,r=l+1,r-1
-        return True                            
+    # def ispali(self,s,l,r):
+    #     while l<r:
+    #         if s[l] != s[r]:
+    #             return False
+    #         l,r=l+1,r-1
+    #     return True                            
         
         
         

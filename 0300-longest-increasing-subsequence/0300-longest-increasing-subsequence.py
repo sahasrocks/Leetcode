1fc978b3a8e1c56fdf5af7bfd870725bc05b3d01
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-10 16:27:57 +0530

    Time: 8581 ms (5%), Space: 246.3 MB (5%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,10 +1,45 @@
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        LIS = [1] * len(nums)
+    def lengthOfLIS(self, nums):
+        n = len(nums)
+        memo = [[-1] * (n + 1) for _ in range(n)]
 
-        for i in range(len(nums) - 1, -1, -1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] < nums[j]:
-                    LIS[i] = max(LIS[i], 1 + LIS[j])
-        return max(LIS)
+        def dfs(i, j):
+            if i == n:
+                return 0
+            if memo[i][j + 1] != -1:
+                return memo[i][j + 1]
+
+            LIS = dfs(i + 1, j)
+
+            if j == -1 or nums[j] < nums[i]:
+                LIS = max(LIS, 1 + dfs(i + 1, i))
+
+            memo[i][j + 1] = LIS
+            return LIS
+
+        return dfs(0, -1)
+# class Solution:
+#     def lengthOfLIS(self, nums: List[int]) -> int:
+#         dp=[[-1] * (len(nums) + 1) for _ in range(len(nums))]
+#         def dfs(i,j):
+#             if i==len(nums):
+#                 return 0
+#             if dp[i][j+1] != -1:
+#                 return dp[i][j+1]
+
+#             lis = dfs(i+1,j)
+#             if j==-1 or nums[j]<nums[i]:
+#                 lis = max(lis,1+dfs(i+1,i))
+#             dp[i][j+1]=lis    
+#             return dp[i][j+1]
+#         return dfs(0,-1)            
+        
+        
+        # LIS = [1] * len(nums)
+
+        # for i in range(len(nums) - 1, -1, -1):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] < nums[j]:
+        #             LIS[i] = max(LIS[i], 1 + LIS[j])
+        # return max(LIS)
         

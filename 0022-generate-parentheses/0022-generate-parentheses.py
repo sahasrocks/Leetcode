1cd6fdb9bbbfc7114dbc2307e8114594c6084344
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-10 18:29:59 +0530

    Time: 1 ms (44.13%), Space: 19.5 MB (5.81%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,19 +1,48 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        stack=[]
         res=[]
-        def backtrack(open,close):
-            if open==close==n:
-                res.append("".join(stack))
+        def bt(cur,open,close):
+            if len(cur) == 2*n:
+                res.append(cur)
                 return
             if open<n:
-                stack.append("(")
-                backtrack(open+1,close)
-                stack.pop()
+                bt(cur+'(',open+1,close)
             if close<open:
-                stack.append(")")
-                backtrack(open,close+1)
-                stack.pop()
-        backtrack(0,0)
+                bt(cur+')',open,close+1)
+        bt("",0,0)
         return res                
+        
+        # stack=[]
+        # res=[]
+        # def bt(open,close):
+        #     if open == close==n:
+        #         res.append("".join(stack))
+        #         return
+        #     if open<n:
+        #         stack.append("(")
+        #         bt(open+1,close)
+        #         stack.pop()
+        #     if close<open:
+        #         stack.append(")")
+        #         bt(open,close+1)
+        #         stack.pop()
+        # bt(0,0)
+        # return res                
+        
+        # stack=[]
+        # res=[]
+        # def backtrack(open,close):
+        #     if open==close==n:
+        #         res.append("".join(stack))
+        #         return
+        #     if open<n:
+        #         stack.append("(")
+        #         backtrack(open+1,close)
+        #         stack.pop()
+        #     if close<open:
+        #         stack.append(")")
+        #         backtrack(open,close+1)
+        #         stack.pop()
+        # backtrack(0,0)
+        # return res                
         

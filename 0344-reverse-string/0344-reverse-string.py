617c130d88304fc0a07df03fb4e19e4ffc72675f
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-10 16:33:04 +0530

    Time: 2 ms (47.72%), Space: 23.5 MB (9.97%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -9,3 +9,8 @@ class Solution:
             s[l],s[r]=s[r],s[l]
             l+=1
             r-=1
+        
+        # while l<=r:
+        #     s[l],s[r]=s[r],s[l]
+        #     l+=1
+        #     r-=1

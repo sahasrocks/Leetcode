99fff4304aee6de3bb06d2bd646d182033736796
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-12 09:11:48 +0530

    Time: 0 ms (100%), Space: 19.3 MB (8.13%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -11,8 +11,15 @@ class Solution:
                 return True
             if not l or not r:
                 return False
-            return (l.val == r.val and dfs(l.left, r.right) and dfs(l.right, r.left))   
-        return dfs(root.left,root.right)         
+            return ((l.val==r.val) and dfs(l.left,r.right) and dfs(l.right,r.left))
+        return dfs(root.left,root.right)            
+        # def dfs(l,r):
+        #     if not l and not r:
+        #         return True
+        #     if not l or not r:
+        #         return False
+        #     return (l.val == r.val and dfs(l.left, r.right) and dfs(l.right, r.left))   
+        # return dfs(root.left,root.right)         
 
 
 

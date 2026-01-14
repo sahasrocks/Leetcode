e3b2e8c58d5d068bdd44b1b97f55e1156ec2d7d1
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-14 09:18:20 +0530

    Time: 18 ms (51.15%), Space: 25.5 MB (6.98%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -4,13 +4,20 @@ from sortedcontainers import SortedList
 class KthLargest(object):
 
     def __init__(self, k, nums):
-        self.k, self.sl = k, SortedList(nums)
-        
+        #self.k, self.sl = k, SortedList(nums)
+        self.k=k
+        self.nums=nums
+        heapq.heapify(self.nums)
+        while len(self.nums)>k:
+            heapq.heappop(self.nums)
 
     def add(self, val):
-        self.sl.add(val)  # Note that sl is a SortedList
-        return self.sl[-self.k]
-
+        #self.sl.add(val)  # Note that sl is a SortedList
+        #return self.sl[-self.k]
+        heapq.heappush(self.nums,val)
+        if len(self.nums)>self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]    
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)

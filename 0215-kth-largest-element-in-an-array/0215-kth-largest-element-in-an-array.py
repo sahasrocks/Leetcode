00bc7d0249cd7f84e354cda2b5731ccaffc87ec9
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-14 09:11:16 +0530

    Time: 63 ms (72.38%), Space: 31.2 MB (11.23%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -6,7 +6,14 @@ class Solution:
                 heapq.heappush(min_heap,n)
             else:
                 heapq.heappushpop(min_heap,n)
-        return min_heap[0]           
+        return min_heap[0]            
+        # min_heap=[]
+        # for n in nums:
+        #     if len(min_heap)<k:
+        #         heapq.heappush(min_heap,n)
+        #     else:
+        #         heapq.heappushpop(min_heap,n)
+        # return min_heap[0]           
 
         # min_heap=[]
         # for num in nums:

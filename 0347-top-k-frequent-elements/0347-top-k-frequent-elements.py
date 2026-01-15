ae04fbd8eb09998eb58b2b52590c5f09d6fe64be
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-15 06:29:40 +0530

    Time: 3 ms (91.45%), Space: 23 MB (21.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,18 +3,32 @@ class Solution:
         # c= Counter(nums)
         
         # return [num for num, _ in c.most_common(k)]
-        count={}
-        for num in nums:
-            count[num]=1+count.get(num,0)
+        c=defaultdict(int)
+        for n in nums:
+            c[n]+=1
         heap=[]
-        for num in count.keys():
-            heapq.heappush(heap,(count[num],num))
+        for num in c.keys():
+            heapq.heappush(heap,(c[num],num))
             if len(heap)>k:
                 heapq.heappop(heap)
         res=[]
         for i in range(k):
             res.append(heapq.heappop(heap)[1])
-        return res                    
+        return res                
+        
+        
+        # count={}
+        # for num in nums:
+        #     count[num]=1+count.get(num,0)
+        # heap=[]
+        # for num in count.keys():
+        #     heapq.heappush(heap,(count[num],num))
+        #     if len(heap)>k:
+        #         heapq.heappop(heap)
+        # res=[]
+        # for i in range(k):
+        #     res.append(heapq.heappop(heap)[1])
+        # return res                    
         
         
         

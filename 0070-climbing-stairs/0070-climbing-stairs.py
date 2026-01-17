6b4ca8b2e377834dc3c47951b83ac6c26ab55b27
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-17 20:48:05 +0530

    Time: 0 ms (100%), Space: 19.3 MB (10.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -12,15 +12,21 @@ class Solution:
         # for i in range(3,n+1):
         #     dp[i]=dp[i-1]+dp[i-2]
         # return dp[n]            
-
-        memo={1:1,2:2}
-        def td(n):
-            if n in memo:
-                return memo[n]
-            else:
-                memo[n]= td(n-1)+td(n-2)
-                return memo[n]
-        return td(n)            
+        if n<=2:
+            return n
+        dp=[0]*(n+1)
+        dp[1],dp[2]=1,2
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]        
+        # memo={1:1,2:2}
+        # def td(n):
+        #     if n in memo:
+        #         return memo[n]
+        #     else:
+        #         memo[n]= td(n-1)+td(n-2)
+        #         return memo[n]
+        # return td(n)            
 
 
 

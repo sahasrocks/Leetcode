3419d913483999a32672144a581c8fb6fa4f2da2
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-17 18:33:29 +0530

    Time: 31 ms (81.02%), Space: 27.8 MB (11.86%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,22 +1,43 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        safe={}
         n=len(graph)
-        indegree=[0]*(n)
-        adj=[[]for _ in range(n)]
-        for u in range(n):
-            for v in graph[u]:
-                adj[v].append(u)
-                indegree[u]+=1
-        queue=deque()
-        for v in range(n):
-            if indegree[v]==0:
-                queue.append(v)
-        ans=[]
-        while queue:
-            u=queue.popleft()
-            ans.append(u)
-            for v in adj[u]:
-                indegree[v]-=1
-                if indegree[v]==0:
-                    queue.append(v)
-        return sorted(ans)
+        def dfs(node):
+            if node in safe:
+                return safe[node]
+            safe[node]=False
+            for nei in graph[node]:
+                if not dfs(nei):
+                    return safe[nei]
+            safe[node]=True
+            return True
+
+        res=[]
+        for i in range(n):
+            if dfs(i):
+                res.append(i)
+        return res                        
+        
+        
+        
+        
+        # n=len(graph)
+        # indegree=[0]*(n)
+        # adj=[[]for _ in range(n)]
+        # for u in range(n):
+        #     for v in graph[u]:
+        #         adj[v].append(u)
+        #         indegree[u]+=1
+        # queue=deque()
+        # for v in range(n):
+        #     if indegree[v]==0:
+        #         queue.append(v)
+        # ans=[]
+        # while queue:
+        #     u=queue.popleft()
+        #     ans.append(u)
+        #     for v in adj[u]:
+        #         indegree[v]-=1
+        #         if indegree[v]==0:
+        #             queue.append(v)
+        # return sorted(ans)

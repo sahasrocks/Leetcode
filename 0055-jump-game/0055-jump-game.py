c6bd4d7306a7b29d809d4521d29df370dfe114c1
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-18 12:20:53 +0530

    Time: 18 ms (70.75%), Space: 20.5 MB (12.42%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,13 +1,16 @@
-class Solution(object):
-    def canJump(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        goal = len(nums)-1
-        for i in range(len(nums)-1,-1,-1):
-            if i + nums[i]>=goal:
-                goal = i
-        return True if goal==0 else False        
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
         
         
+        j=0
+        for i in range(len(nums)):
+            if i>j:
+                return False
+            j=max(j,i+nums[i])
+        return True        
+        # j=0
+        # for i in range(len(nums)):
+        #     if i>j:
+        #         return False
+        #     j=max(j,i+nums[i])
+        # return True

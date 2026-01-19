b5ecfcd1daebf813ec6439762315b53a9ee23e01
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-19 05:54:25 +0530

    Time: 1320 ms (34.19%), Space: 171.1 MB (9.88%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,15 +1,32 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2:
+        if sum(nums)%2:
             return False
-        dp=set()
-        dp.add(0)
         target=sum(nums)//2
-        for i in range(len(nums)-1,-1,-1):
-            nextDP=set()
-            for j in dp:
-                nextDP.add(j+nums[i])
-                nextDP.add(j)
-            dp=nextDP
-        return True if target in dp else False            
+        n=len(nums)
+        dp=[[-1]*(target+1) for i in range(n+1)]
+        def dfs(i,target):
+            if target==0:
+                return True
+            if i>=n or target<0:
+                return False
+            if dp[i][target] != -1:
+                return dp[i][target]
+            dp[i][target] = dfs(i+1,target) or dfs(i+1,target-nums[i])
+            return dp[i][target]
+        return dfs(0,target)                    
+        
+        
+        # if sum(nums) % 2:
+        #     return False
+        # dp=set()
+        # dp.add(0)
+        # target=sum(nums)//2
+        # for i in range(len(nums)-1,-1,-1):
+        #     nextDP=set()
+        #     for j in dp:
+        #         nextDP.add(j+nums[i])
+        #         nextDP.add(j)
+        #     dp=nextDP
+        # return True if target in dp else False            
         

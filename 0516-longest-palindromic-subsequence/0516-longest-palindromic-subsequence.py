c5a81f0adcb669cef1ee541c53154663bc52d50e
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-01-19 06:42:41 +0530

    Time: 731 ms (69.66%), Space: 257.1 MB (5.2%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,19 +1,34 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        memo={}
+        dp={}
         def dfs(i,j):
             if i>j:
                 return 0
             if i==j:
                 return 1
-            if (i,j) in memo:
-                return memo[(i,j)]
+            if (i,j) in dp:
+                return dp[(i,j)]
             if s[i]==s[j]:
-                memo[(i,j)] = dfs(i+1,j-1)+2
+                dp[(i,j)] = dfs(i+1,j-1)+2
             else:
-                memo[(i,j)] = max(dfs(i+1,j),dfs(i,j-1))
-            return memo[(i,j)]
+                dp[(i,j)] = max(dfs(i+1,j),dfs(i,j-1))
+            return dp[(i,j)]
         return dfs(0,len(s)-1)                        
+        
+        # memo={}
+        # def dfs(i,j):
+        #     if i>j:
+        #         return 0
+        #     if i==j:
+        #         return 1
+        #     if (i,j) in memo:
+        #         return memo[(i,j)]
+        #     if s[i]==s[j]:
+        #         memo[(i,j)] = dfs(i+1,j-1)+2
+        #     else:
+        #         memo[(i,j)] = max(dfs(i+1,j),dfs(i,j-1))
+        #     return memo[(i,j)]
+        # return dfs(0,len(s)-1)                        
         
         
         # rev_s = s[::-1]

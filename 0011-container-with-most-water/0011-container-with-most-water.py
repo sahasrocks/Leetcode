f4711e73d9885cb7c04fa2c2d51e35f59d260148
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-02 05:21:24 +0530

    Time: 59 ms (74.25%), Space: 29.7 MB (17.4%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,15 +1,110 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        l,r=0,len(height)-1
+        l=0
+        r=len(height)-1
         res=0
         while l<r:
             a=min(height[l],height[r])*(r-l)
             res=max(res,a)
-            if height[l]<=height[r]:
+            if height[l]<height[r]:
                 l+=1
             else:
                 r-=1
         return res            
+        
+        
+        
+        # l=0
+        # r=len(height)-1
+        # res=0
+        # while l<r:
+        #     a=min(height[l],height[r])*(r-l)
+        #     res=max(res,a)
+        #     if height[l]<height[r]:
+        #         l+=1
+        #     else:
+        #         r-=1
+        # return res        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # l=0
+        # r=len(height)-1
+        # res=0
+        # while l<r:
+        #     a=min(height[l],height[r]) *(r-l)
+        #     res=max(res,a)
+        #     if height[l]<height[r]:
+        #         l+=1
+        #     else:
+        #         r-=1
+        # return res            
+        
+        
+        # l=0
+        # r=len(height)-1
+        # res=0
+        # while l<r:
+        #     a=min(height[l],height[r])*(r-l)
+        #     res=max(res,a)
+        #     if height[l]<height[r]:
+        #         l+=1
+        #     else:
+        #         r-=1
+        # return res            
+        
+        
+        
+        
+        
+        
+        # l,r=0,len(height)-1
+        # res=0
+        # while l<r:
+        #     a=min(height[l],height[r])*(r-l)
+        #     res=max(res,a)
+        #     if height[l]<height[r]:
+        #         l+=1
+        #     else:
+        #         r-=1
+        # return res            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # l,r=0,len(height)-1
+        # res=0
+        # while l<r:
+        #     a=min(height[l],height[r])*(r-l)
+        #     res=max(res,a)
+        #     if height[l]<=height[r]:
+        #         l+=1
+        #     else:
+        #         r-=1
+        # return res            
         
         
         

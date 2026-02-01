99cc7ee9852abc1d0825c097c89d5e73a1e3a735
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-02 05:14:33 +0530

    Time: 3 ms (76.21%), Space: 26.6 MB (21.99%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -16,6 +16,19 @@ class Solution:
         if k>0:
             swap(nums,0,len(nums)-1)
             swap(nums,0,k-1)
-            swap(nums,k,len(nums)-1)               
+            swap(nums,k,len(nums)-1)            
+        
+        # def swap(arr,i,j):
+        #     while i<j:
+        #         arr[i],arr[j]=arr[j],arr[i]
+        #         i+=1
+        #         j-=1
+        #     return arr
+        # if k>len(nums):
+        #     k=k%len(nums)
+        # if k>0:
+        #     swap(nums,0,len(nums)-1)
+        #     swap(nums,0,k-1)
+        #     swap(nums,k,len(nums)-1)               
               
         

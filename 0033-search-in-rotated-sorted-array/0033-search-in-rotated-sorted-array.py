8cf40a332ccc21b4360418cda2154d402651291d
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-02 06:51:31 +0530

    Time: 0 ms (100%), Space: 19.6 MB (19.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -11,11 +11,37 @@ class Solution:
                 else:
                     r=m-1
             else:
-                if target < nums[m] or target> nums[r]:
+                if target<nums[m] or target>nums[r]:
                     r=m-1
                 else:
-                    l=m+1
-        return -1                                
+                    l=m+1 
+        return -1                              
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # l,r=0,len(nums)-1
+        # while l<=r:
+        #     m=(l+r)//2
+        #     if nums[m]==target:
+        #         return m
+        #     if nums[l]<=nums[m]:
+        #         if target>nums[m] or target<nums[l]:
+        #             l=m+1
+        #         else:
+        #             r=m-1
+        #     else:
+        #         if target < nums[m] or target> nums[r]:
+        #             r=m-1
+        #         else:
+        #             l=m+1
+        # return -1                                
         
         
         

6b588d43a3774123974bcbb635c418379ce9d22c
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-02 06:21:15 +0530

    Time: 0 ms (100%), Space: 19.5 MB (21.25%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -10,6 +10,42 @@ class Solution:
             else:
                 return m        
         
+        
+        
+        
+        
+        
+        # l,r=0,len(nums)-1
+        # while l<=r:
+        #     m=(l+r)//2
+        #     if m>0 and nums[m]<nums[m-1]:
+        #         r=m-1
+        #     elif m<len(nums)-1 and nums[m]<nums[m+1]:
+        #         l=m+1
+        #     else:
+        #         return m        
+        
+        
+        # l,r=0,len(nums)-1
+        # while l<=r:
+        #     m=(l+r)//2
+        #     if m>0 and nums[m]<nums[m-1]:
+        #         r=m-1
+        #     elif m<len(nums)-1 and nums[m]<nums[m+1]:
+        #         l=m+1
+        #     else:
+        #         return m        
+        
+        # l,r=0,len(nums)-1
+        # while l<=r:
+        #     m=(l+r)//2
+        #     if m>0 and nums[m]<nums[m-1]:
+        #         r=m-1
+        #     elif m<len(nums)-1 and nums[m]<nums[m+1]:
+        #         l=m+1
+        #     else:
+        #         return m        
+        
         # l,r=0,len(nums)-1
         # while l<=r:
         #     m=(l+r)//2

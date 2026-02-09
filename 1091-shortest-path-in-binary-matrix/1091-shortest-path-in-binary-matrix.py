12d0f71e2617460e3aadf78ea67a52d27672908e
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-09 17:31:49 +0530

    Time: 311 ms (7.69%), Space: 20.8 MB (27.33%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,6 +1,6 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        n= len (grid)
+        n=len(grid)
         if grid[0][0] or grid[n-1][n-1]:
             return -1
         q=deque([(0,0,1)])
@@ -11,8 +11,26 @@ class Solution:
             if r==n-1 and c==n-1:
                 return length
             for dr,dc in dirs:
-                nr,nc=r+dr,c+dc
-                if ( 0<=nr<n and 0<=nc<n  and grid[nr][nc] ==0 and (nr,nc) not in visited ):
+                nr,nc=dr+r,dc+c
+                if (nr in range(n) and nc in range(n) and (nr,nc) not in visited and grid[nr][nc]==0):
                     q.append((nr,nc,length+1))
                     visited.add((nr,nc))
-        return -1               
+        return -1                
+
+        
+        # n= len (grid)
+        # if grid[0][0] or grid[n-1][n-1]:
+        #     return -1
+        # q=deque([(0,0,1)])
+        # visited=set((0,0))
+        # dirs=[(0,1),(1,0),(0,-1),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
+        # while q:
+        #     r,c,length=q.popleft()
+        #     if r==n-1 and c==n-1:
+        #         return length
+        #     for dr,dc in dirs:
+        #         nr,nc=r+dr,c+dc
+        #         if ( 0<=nr<n and 0<=nc<n  and grid[nr][nc] ==0 and (nr,nc) not in visited ):
+        #             q.append((nr,nc,length+1))
+        #             visited.add((nr,nc))
+        # return -1               

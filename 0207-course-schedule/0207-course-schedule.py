f7c4ad72c8509d343bea1a940e80e132491ad1fc
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-09 16:45:54 +0530

    Time: 7 ms (45.26%), Space: 21.4 MB (12.73%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -24,26 +24,71 @@
 #             return True                            
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap=defaultdict(list)
-        for crs, pre in prerequisites:
-            preMap[crs].append(pre)
+        premap=defaultdict(list)
+        for crs,pre in prerequisites:
+            premap[crs].append(pre)
         visited=set()
         def dfs(crs):
             if crs in visited:
                 return False
-            if preMap[crs] == []:
+            if premap[crs]==[]:
                 return True
             visited.add(crs)
-            for pre in preMap[crs]:
+            for pre in premap[crs]:
                 if not dfs(pre):
                     return False
             visited.remove(crs)
-            preMap[crs] =[]
+            premap[crs]=[]
             return True
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
-        return True                                
+        return True        
+
+        
+        
+        # premap=defaultdict(list)
+        # for crs,pre in prerequisites:
+        #     premap[crs].append(pre)
+        # visited=set()
+        # def dfs(crs):
+        #     if crs in visited:
+        #         return False
+        #     if premap[crs]==[]:
+        #         return True
+        #     visited.add(crs)
+        #     for pre in premap[crs]:
+        #         if not dfs(pre):
+        #             return False
+        #     visited.remove(crs)
+        #     premap[crs] =[]
+        #     return True
+        # print(premap)    
+        # for crs in range(numCourses):
+        #     if not dfs(crs):
+        #         return False
+        # return True                               
+        
+        # preMap=defaultdict(list)
+        # for crs, pre in prerequisites:
+        #     preMap[crs].append(pre)
+        # visited=set()
+        # def dfs(crs):
+        #     if crs in visited:
+        #         return False
+        #     if preMap[crs] == []:
+        #         return True
+        #     visited.add(crs)
+        #     for pre in preMap[crs]:
+        #         if not dfs(pre):
+        #             return False
+        #     visited.remove(crs)
+        #     preMap[crs] =[]
+        #     return True
+        # for crs in range(numCourses):
+        #     if not dfs(crs):
+        #         return False
+        # return True                                
         
         
         # preMap = {i: [] for i in range(numCourses)}

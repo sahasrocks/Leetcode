ff373e4b0bf9afcc8a6f9d7dd6e4bde13604a5a5
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-10 05:09:52 +0530

    Time: 326 ms (41.69%), Space: 22 MB (18.66%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,6 +1,6 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        rows,cols=len(heights),len(heights[0])
+        m,n=len(heights),len(heights[0])
         visit=set()
         minHeap=[(0,0,0)]
         dirs=[(1,0),(0,1),(-1,0),(0,-1)]
@@ -9,13 +9,31 @@ class Solution:
             if (r,c) in visit:
                 continue
             visit.add((r,c))
-            if (r,c)==(rows-1,cols-1):
+            if (r,c)==(m-1,n-1):
                 return diff
             for dr,dc in dirs:
-                nr,nc = dr+r,dc+c
-                if ( nr<0 or nr>=rows or nc<0 or nc>=cols or (nr,nc) in visit ):
-                    continue
-                newDiff = max(diff,abs(heights[r][c]-heights[nr][nc]))
-                heapq.heappush(minHeap,(newDiff,nr,nc))
-        return 0                
+                nr,nc = r+dr,c+dc
+                if (nr in range(m) and nc in range(n) and (nr,nc) not in visit):
+                    newDiff=max(diff,abs(heights[r][c]-heights[nr][nc]))
+                    heapq.heappush(minHeap,(newDiff,nr,nc))
+        return 0                    
+        
+        # rows,cols=len(heights),len(heights[0])
+        # visit=set()
+        # minHeap=[(0,0,0)]
+        # dirs=[(1,0),(0,1),(-1,0),(0,-1)]
+        # while minHeap:
+        #     diff,r,c=heapq.heappop(minHeap)
+        #     if (r,c) in visit:
+        #         continue
+        #     visit.add((r,c))
+        #     if (r,c)==(rows-1,cols-1):
+        #         return diff
+        #     for dr,dc in dirs:
+        #         nr,nc = dr+r,dc+c
+        #         if ( nr<0 or nr>=rows or nc<0 or nc>=cols or (nr,nc) in visit ):
+        #             continue
+        #         newDiff = max(diff,abs(heights[r][c]-heights[nr][nc]))
+        #         heapq.heappush(minHeap,(newDiff,nr,nc))
+        # return 0                
         

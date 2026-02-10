9473897f29f46478991e1f16196c3bfc226cc8a6
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-11 05:18:25 +0530

    Time: 0 ms (100%), Space: 19.5 MB (6.63%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,18 +1,30 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
     
-        def helper(nums):
+        def dfs(nums):
             if not nums:
                 return 0
             if len(nums)==1:
                 return nums[0]
-            dp=[0]*(len(nums))
+            dp=[0]*len(nums)
             dp[0]=nums[0]
             dp[1]=max(nums[0],nums[1])
             for i in range(2,len(nums)):
                 dp[i]=max(dp[i-1],nums[i]+dp[i-2])
             return dp[-1]
-        return max(nums[0],helper(nums[1:]),helper(nums[:-1]))    
+        return max(nums[0],dfs(nums[1:]),dfs(nums[:-1]))                
+        # def helper(nums):
+        #     if not nums:
+        #         return 0
+        #     if len(nums)==1:
+        #         return nums[0]
+        #     dp=[0]*(len(nums))
+        #     dp[0]=nums[0]
+        #     dp[1]=max(nums[0],nums[1])
+        #     for i in range(2,len(nums)):
+        #         dp[i]=max(dp[i-1],nums[i]+dp[i-2])
+        #     return dp[-1]
+        # return max(nums[0],helper(nums[1:]),helper(nums[:-1]))    
 
     
     

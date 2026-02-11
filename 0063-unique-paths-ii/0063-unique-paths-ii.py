76bcfeb516a76d7ffc75b0fac1a3dea20de4ec85
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-11 16:43:42 +0530

    Time: 3 ms (24.59%), Space: 19.5 MB (21.07%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,19 +1,35 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m,n=len(obstacleGrid),len(obstacleGrid[0])
-        if obstacleGrid[m-1][n-1] ==1:
+        if obstacleGrid[m-1][n-1]==1:
             return 0
         dp=[[-1]*n for i in range(m)]
         def dfs(i,j):
-            if i==(m-1) and j==n-1:
+            if i==(m-1) and j==(n-1):
                 return 1
             if i>=m or j>=n or obstacleGrid[i][j]:
                 return 0
-            if dp[i][j] != -1:
-                return dp[i][j]
-            dp[i][j] = dfs(i+1,j) + dfs(i,j+1)
+            if dp[i][j] !=-1:
+                return dp[i][j]    
+            dp[i][j]=dfs(i+1,j)+dfs(i,j+1)
             return dp[i][j]
-        return dfs(0,0)                    
+        return   dfs(0,0)              
+        
+        
+        # m,n=len(obstacleGrid),len(obstacleGrid[0])
+        # if obstacleGrid[m-1][n-1] ==1:
+        #     return 0
+        # dp=[[-1]*n for i in range(m)]
+        # def dfs(i,j):
+        #     if i==(m-1) and j==n-1:
+        #         return 1
+        #     if i>=m or j>=n or obstacleGrid[i][j]:
+        #         return 0
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+        #     dp[i][j] = dfs(i+1,j) + dfs(i,j+1)
+        #     return dp[i][j]
+        # return dfs(0,0)                    
         
         
         # m=len(obstacleGrid)

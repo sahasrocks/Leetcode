317fed2a7d95ecd5943b7fe1e3984ba0e6376c7c
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-12 05:05:05 +0530

    Time: 1375 ms (7.85%), Space: 316.4 MB (7.61%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -6,12 +6,24 @@ class Solution:
                 return 0
             if (i,j) in dp:
                 return dp[(i,j)]
-            if text1[i] == text2[j]:
-                dp[(i,j)] = 1+ dfs(i+1,j+1) 
-            else:
-                dp[(i,j)] = max(dfs(i+1,j), dfs(i,j+1))  
+            if text1[i]==text2[j]:
+                dp[(i,j)]=1 + dfs(i+1,j+1)
+            else:    
+                dp[(i,j)]=max(dfs(i+1,j),dfs(i,j+1))
             return dp[(i,j)]
         return dfs(0,0)                
+        # dp={}
+        # def dfs(i,j):
+        #     if i==len(text1) or j==len(text2):
+        #         return 0
+        #     if (i,j) in dp:
+        #         return dp[(i,j)]
+        #     if text1[i] == text2[j]:
+        #         dp[(i,j)] = 1+ dfs(i+1,j+1) 
+        #     else:
+        #         dp[(i,j)] = max(dfs(i+1,j), dfs(i,j+1))  
+        #     return dp[(i,j)]
+        # return dfs(0,0)                
         
         # dp={}
         # def dfs(i,j):

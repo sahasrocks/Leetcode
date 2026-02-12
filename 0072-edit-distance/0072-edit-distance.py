1783e0c9206fd35af4bc3841ea625cd4049529b2
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-12 05:57:03 +0530

    Time: 35 ms (86.18%), Space: 24.3 MB (16.5%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -10,13 +10,30 @@ class Solution:
             if (i,j) in dp:
                 return dp[(i,j)]
             if word1[i]==word2[j]:
-                dp[(i,j)]=dfs(i+1,j+1)
+                dp[(i,j)] = dfs(i+1,j+1) 
             else:
                 res=min(dfs(i+1,j),dfs(i,j+1))
                 res=min(res,dfs(i+1,j+1))
-                dp[(i,j)]=res+1
+                dp[(i,j)] = res+1
             return dp[(i,j)]
-        return dfs(0,0)                        
+        return dfs(0,0)                       
+        # m,n=len(word1),len(word2)
+        # dp={}
+        # def dfs(i,j):
+        #     if i==m:
+        #         return n-j
+        #     if j==n:
+        #         return m-i
+        #     if (i,j) in dp:
+        #         return dp[(i,j)]
+        #     if word1[i]==word2[j]:
+        #         dp[(i,j)]=dfs(i+1,j+1)
+        #     else:
+        #         res=min(dfs(i+1,j),dfs(i,j+1))
+        #         res=min(res,dfs(i+1,j+1))
+        #         dp[(i,j)]=res+1
+        #     return dp[(i,j)]
+        # return dfs(0,0)                        
         
         
         

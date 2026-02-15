3356b4f5274e87c36991a8a3306981b6268d424c
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-16 05:12:17 +0530

    Time: 143 ms (75.57%), Space: 32 MB (54.17%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -6,7 +6,32 @@ class Solution:
         nums.sort()
         for i in range(len(nums)-1):
             dp[i]=nums[i+1]-nums[i]
-        return max(dp)    
+        return max(dp)        
+        # dp=[0]*len(nums)
+        # if len(nums)<2:
+        #     return 0
+        # nums.sort()
+        # for i in range(len(nums)-1):
+        #     dp[i]=nums[i+1]-nums[i]
+        # return max(dp)        
+        
+        
+        
+        # dp=[0]*len(nums)
+        # if len(nums)<2:
+        #     return 0
+        # nums.sort()
+        # for i in range(len(nums)-1):
+        #     dp[i]=nums[i+1]-nums[i]
+        # return max(dp)        
+        
+        # dp=[0]*len(nums)
+        # if len(nums)<2:
+        #     return 0
+        # nums.sort()
+        # for i in range(len(nums)-1):
+        #     dp[i]=nums[i+1]-nums[i]
+        # return max(dp)    
         
         # res=0
         # i=1

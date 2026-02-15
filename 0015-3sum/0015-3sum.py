cc38dcd95eb7e830bf3ab4a058dc309ffb50e734
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-16 04:24:30 +0530

    Time: 439 ms (94.31%), Space: 21.9 MB (70.44%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -9,18 +9,70 @@ class Solution:
                 continue
             l,r=i+1,len(nums)-1
             while l<r:
-                ts=nums[l]+nums[r]+e
-                if ts>0:
-                    r-=1
-                elif ts<0:
+                ts=e+nums[l]+nums[r]
+                if ts<0:
                     l+=1
+                elif ts>0:
+                    r-=1
                 else:
-                    res.append([e,nums[l],nums[r]])
+                    res.append((e,nums[l],nums[r]))
                     l+=1
                     r-=1
                     while nums[l]==nums[l-1] and l<r:
                         l+=1
         return res                                
+        
+        
+        
+        # res=[]
+        # nums.sort()
+        # for i,e in enumerate(nums):
+        #     if e>0:
+        #         break
+        #     if i>0 and e==nums[i-1]:
+        #         continue
+        #     l,r=i+1,len(nums)-1
+        #     while l<r:
+        #         ts=nums[l]+nums[r]+e
+        #         if ts<0:
+        #             l+=1
+        #         elif ts>0:
+        #             r-=1
+        #         else:
+        #             res.append([e,nums[l],nums[r]])
+        #             l+=1
+        #             r-=1
+        #             while nums[l]==nums[l-1] and l<r:
+        #                 l+=1
+        # return res                                
+        
+        
+        
+        
+        
+        
+        
+        # res=[]
+        # nums.sort()
+        # for i,e in enumerate(nums):
+        #     if e>0:
+        #         break
+        #     if i>0 and e==nums[i-1]:
+        #         continue
+        #     l,r=i+1,len(nums)-1
+        #     while l<r:
+        #         ts=nums[l]+nums[r]+e
+        #         if ts>0:
+        #             r-=1
+        #         elif ts<0:
+        #             l+=1
+        #         else:
+        #             res.append([e,nums[l],nums[r]])
+        #             l+=1
+        #             r-=1
+        #             while nums[l]==nums[l-1] and l<r:
+        #                 l+=1
+        # return res                                
         
         
         

8f79f3ee7c87305831c73df590fd79b69a431b04
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-16 07:21:53 +0530

    Time: 27 ms (93.82%), Space: 19.6 MB (34.24%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -8,6 +8,25 @@ class Solution:
             else:
                 return s[l:r]==s[l:r][::-1] or s[l+1:r+1]==s[l+1:r+1][::-1]
         return True            
+        
+        # l,r=0,len(s)-1
+        # while l<r:
+        #     if s[l]==s[r]:
+        #         l+=1
+        #         r-=1
+        #     else:
+        #         return s[l:r]==s[l:r][::-1] or s[l+1:r+1]==s[l+1:r+1][::-1]
+        # return True            
+        
+        
+        # l,r=0,len(s)-1
+        # while l<r:
+        #     if s[l]==s[r]:
+        #         l+=1
+        #         r-=1
+        #     else:
+        #         return s[l:r]==s[l:r][::-1] or s[l+1:r+1]==s[l+1:r+1][::-1]
+        # return True            
 
         
         

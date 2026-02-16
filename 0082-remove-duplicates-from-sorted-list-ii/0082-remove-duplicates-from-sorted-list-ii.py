77f6562d4b7dfbd2950132bdd2df64bfc70e501f
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-16 19:00:58 +0530

    Time: 0 ms (100%), Space: 19.5 MB (5.03%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -5,15 +5,45 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        d=ListNode()
-        d.next = head
-        cur =d
+        dummy=ListNode(0,head)
+        cur=dummy
         while cur:
             if cur.next and cur.next.next and cur.next.val ==cur.next.next.val:
                 temp=cur.next.next
-                while temp and temp.next and temp.val ==temp.next.val:
+                while temp and temp.next and temp.val == temp.next.val:
                     temp=temp.next
                 cur.next=temp.next
             else:
                 cur=cur.next
-        return d.next                
+        return dummy.next                
+        
+        
+        # dummyNode=ListNode()
+        # dummyNode.next=head
+        # curr=dummyNode
+        # while curr:
+        #     if curr.next and curr.next.next and curr.next.val==curr.next.next.val:
+        #         temp=curr.next.next
+        #         while temp and temp.next and temp.val==temp.next.val:
+        #             temp=temp.next
+        #         curr.next=temp.next
+        #     else:
+        #         curr=curr.next
+        # return dummyNode.next                
+
+
+
+
+
+        # d=ListNode()
+        # d.next = head
+        # cur =d
+        # while cur:
+        #     if cur.next and cur.next.next and cur.next.val ==cur.next.next.val:
+        #         temp=cur.next.next
+        #         while temp and temp.next and temp.val ==temp.next.val:
+        #             temp=temp.next
+        #         cur.next=temp.next
+        #     else:
+        #         cur=cur.next
+        # return d.next                

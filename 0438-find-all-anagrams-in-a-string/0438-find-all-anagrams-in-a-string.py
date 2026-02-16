3109146bea66d5f0c1652a72f272792d739838fe
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-16 06:33:18 +0530

    Time: 39 ms (66.18%), Space: 19.6 MB (66.56%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,26 +1,64 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        
-        scount={}
-        pcount={}
+        scount,pcount=defaultdict(int),defaultdict(int)
         if len(p)>len(s):
             return []
         for i in range(len(p)):
-            pcount[p[i]]= 1+ pcount.get(p[i],0)
-            scount[s[i]]=1+scount.get(s[i],0)
-
-        res=[0] if pcount == scount else []
+            pcount[p[i]]+=1
+            scount[s[i]]+=1
+        res=[0] if pcount==scount else []
         l=0
-        for r in range(len(p),len(s)):
-            scount[s[r]] = 1 + scount.get(s[r],0)
-            scount[s[l]] -= 1
-
+        for i in range(len(p),len(s)):
+            scount[s[i]]+=1
+            scount[s[l]]-=1
             if scount[s[l]]==0:
                 scount.pop(s[l])
             l+=1
-            if scount==pcount:
+            if pcount==scount:
                 res.append(l)
-        return res            
+        return res                    
+        
+        # scount={}
+        # pcount={}
+        # if len(p)>len(s):
+        #     return []
+        # for i in range(len(p)):
+        #     pcount[p[i]]=1+pcount.get(p[i],0)
+        #     scount[s[i]]=1+scount.get(s[i],0)
+        # res=[0] if pcount==scount else []
+        # l=0
+        # for i in range(len(p),len(s)):
+        #     scount[s[i]] =1 +scount.get(s[i],0)
+        #     scount[s[l]] -=1
+        #     if scount[s[l]]==0:
+        #         scount.pop(s[l])
+        #     l+=1
+        #     if pcount==scount:
+        #         res.append(l)
+        # return res                      
+
+
+
+        # scount={}
+        # pcount={}
+        # if len(p)>len(s):
+        #     return []
+        # for i in range(len(p)):
+        #     pcount[p[i]]= 1+ pcount.get(p[i],0)
+        #     scount[s[i]]=1+scount.get(s[i],0)
+
+        # res=[0] if pcount == scount else []
+        # l=0
+        # for r in range(len(p),len(s)):
+        #     scount[s[r]] = 1 + scount.get(s[r],0)
+        #     scount[s[l]] -= 1
+
+        #     if scount[s[l]]==0:
+        #         scount.pop(s[l])
+        #     l+=1
+        #     if scount==pcount:
+        #         res.append(l)
+        # return res            
 
 
 

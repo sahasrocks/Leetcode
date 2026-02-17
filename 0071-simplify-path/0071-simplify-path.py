43e0346589afd989cd04386d602175b264c51e9c
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-17 18:33:43 +0530

    Time: 4 ms (16.92%), Space: 19.3 MB (47.91%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,10 +1,26 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         ans=[]
-        for p in path.split("/"):
+        for p in path.split('/'):
             if p==".." and ans:
                 ans.pop()
             elif p not in "..":
-                ans.append(p)
-        #print(ans)
-        return "/"+"/".join(ans)            
+                ans.append(p)    
+        return "/"+"/".join(ans)    
+
+        # ans=[]
+        # for p in path.split('/'):
+        #     if p ==".." and ans:
+        #         ans.pop()
+        #     elif p not in "..":
+        #         ans.append(p)
+        # return "/"+"/".join(ans)            
+        
+        # ans=[]
+        # for p in path.split("/"):
+        #     if p==".." and ans:
+        #         ans.pop()
+        #     elif p not in "..":
+        #         ans.append(p)
+        # #print(ans)
+        # return "/"+"/".join(ans)            

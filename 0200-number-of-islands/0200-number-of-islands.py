2f209c757a92174ba466f0a175e576989c5a3e2f
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-18 08:30:41 +0530

    Time: 241 ms (65.61%), Space: 21.6 MB (77.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,7 +2,7 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n=len(grid),len(grid[0])
         def dfs(i,j):
-            if i<0 or i>=m or j<0 or j>=n or grid[i][j] !='1':
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j] != '1':
                 return
             else:
                 grid[i][j]='0'
@@ -13,10 +13,46 @@ class Solution:
         num_i=0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] =='1':
+                if grid[i][j]=='1':
                     num_i+=1
                     dfs(i,j)
-        return num_i                        
+        return num_i                       
+        
+        # m,n=len(grid),len(grid[0])
+        # def dfs(i,j):
+        #     if i<0 or i>=m or j<0 or j>=n or grid[i][j] !='1':
+        #         return
+        #     else:
+        #         grid[i][j]='0'
+        #         dfs(i,j+1)
+        #         dfs(i+1,j)
+        #         dfs(i,j-1)
+        #         dfs(i-1,j)
+        # num_i=0
+        # for i in range(m):
+        #     for j in range(n):
+        #         if grid[i][j] =='1':
+        #             num_i+=1
+        #             dfs(i,j)
+        # return num_i                    
+
+        # m,n=len(grid),len(grid[0])
+        # def dfs(i,j):
+        #     if i<0 or i>=m or j<0 or j>=n or grid[i][j] !='1':
+        #         return
+        #     else:
+        #         grid[i][j]='0'
+        #         dfs(i,j+1)
+        #         dfs(i+1,j)
+        #         dfs(i,j-1)
+        #         dfs(i-1,j)
+        # num_i=0
+        # for i in range(m):
+        #     for j in range(n):
+        #         if grid[i][j] =='1':
+        #             num_i+=1
+        #             dfs(i,j)
+        # return num_i                        
         
         # m,n=len(grid),len(grid[0])
         # def dfs(i,j):

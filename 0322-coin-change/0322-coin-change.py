b271f013133f9c6528e6994df41bce13e622e211
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-02-19 07:04:52 +0530

    Time: 804 ms (30.36%), Space: 63.3 MB (15.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -43,19 +43,30 @@ class Solution:
         
         # minimum = check(amount)
         # return minimum if minimum < float("inf") else -1
+        # dp={}
+        # def dfs(i):
+        #     if i in dp:
+        #         return dp[i]
+        #     if i==0:
+        #         return 0
+        #     if i<0:
+        #         return float('inf')
+        #     dp[i]=min([1+dfs(i-coin) for coin in coins])
+        #     return dp[i]
+        # mi=dfs(amount)
+        # return mi if mi <float('inf') else -1
         dp={}
         def dfs(i):
-            if i in dp:
-                return dp[i]
             if i==0:
                 return 0
+            if i in dp:
+                return dp[i]
             if i<0:
                 return float('inf')
-            dp[i]=min([1+dfs(i-coin) for coin in coins])
+            dp[i] = min([1+dfs(i-coin) for coin in coins])
             return dp[i]
-        mi=dfs(amount)
-        return mi if mi <float('inf') else -1
- 
+        mi= dfs(amount)                
+        return mi if mi < float('inf') else -1
         # dp={}
         # def dfs(i):
         #     if i in dp:

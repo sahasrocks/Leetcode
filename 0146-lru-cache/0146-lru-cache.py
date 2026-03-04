2c0ff5918c156b4d7b181ab8b05c4c6d47b6716f
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-03-04 07:51:18 +0530

    Time: 113 ms (71.59%), Space: 76.6 MB (95.59%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,43 +1,27 @@
-class ListNode:
-    def __init__(self, key=0, val=0, prev=None, next=None):
-        self.key = key
-        self.val = val
-        self.prev = prev
-        self.next = next
-        
+from collections import OrderedDict
 
 class LRUCache:
+    __slots__ = ('data', 'capacity')
 
     def __init__(self, capacity: int):
-        """Initialize hash table & dll"""
-        self.cpty = capacity
-        self.htab = dict() #hash table 
-        self.head = ListNode() #doubly linked list
-        self.tail = ListNode()
-        self.head.next = self.tail
-        self.tail.prev = self.head 
-        
-    def _del(self, key: int) -> int: 
-        """Delete given key from hash table & dll"""
-        node = self.htab.pop(key)
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        return node.val
+        self.data: Dict[int, int] = OrderedDict()
+        self.capacity: int = capacity
 
-    def _ins(self, key: int, value: int) -> None: 
-        """Insert at tail"""
-        node = ListNode(key, value, self.tail.prev, self.tail)
-        self.tail.prev.next = self.tail.prev = node
-        self.htab[key] = node
-        
     def get(self, key: int) -> int:
-        if key not in self.htab: return -1
-        value = self._del(key)
-        self._ins(key, value)
-        return value
+        return -1 if key not in self.data else self.data.setdefault(key, self.data.pop(key))
 
     def put(self, key: int, value: int) -> None:
-        if key in self.htab: self._del(key)
-        self._ins(key, value)
-        if len(self.htab) > self.cpty: 
-            self._del(self.head.next.key)
+        try:
+            self.data.move_to_end(key)
+            self.data[key] = value
+        except KeyError:
+            self.data[key] = value
+            if len(self.data) > self.capacity:
+                self.data.popitem(last=False)
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)

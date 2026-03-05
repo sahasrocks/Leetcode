54d737b9ed1be417078f5905923aa4072f49a6c9
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-03-06 04:20:49 +0530

    Time: 7 ms (28.59%), Space: 20.5 MB (91.37%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -14,6 +14,16 @@ class Solution:
         #     else:
         #         r-=1
 
+        l,r=0,len(numbers)-1
+        while l<=r:
+            s=numbers[l]+numbers[r]
+            if s==target:
+                return [l+1,r+1]
+            elif s<target:
+                l+=1
+            else:
+                r-=1        
+
 
 
 

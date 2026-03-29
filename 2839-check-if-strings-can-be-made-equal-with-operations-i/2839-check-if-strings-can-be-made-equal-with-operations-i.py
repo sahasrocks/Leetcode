24ef75315654a558cf1fee5cac1e173c596bcad7
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-03-29 11:41:54 +0530

    Time: 0 ms (100%), Space: 19.3 MB (74.85%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        # check even positions
+        even = (s1[0] == s2[0] and s1[2] == s2[2]) or \
+               (s1[0] == s2[2] and s1[2] == s2[0])
+
+        # check odd positions
+        odd = (s1[1] == s2[1] and s1[3] == s2[3]) or \
+              (s1[1] == s2[3] and s1[3] == s2[1])
+
+        return even and odd

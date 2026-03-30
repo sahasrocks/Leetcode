ac90fdeed097cdd0ad8d5f20170c2aff70dd9c17
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-03-30 16:42:43 +0530

    Time: 24 ms (93.38%), Space: 20.7 MB (75.43%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,6 +1,5 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        
         stack=[]
         s=list(s)
         for i,e in enumerate(s):
@@ -13,7 +12,23 @@ class Solution:
                     s[i]=''
         while stack:
             s[stack.pop()]=''
-        return ''.join(s)                    
+        return ''.join(s)            
+
+
+
+        # stack=[]
+        # s=list(s)
+        # for i,e in enumerate(s):
+        #     if e=='(':
+        #         stack.append(i)
+        #     elif e==')':
+        #         if stack:
+        #             stack.pop()
+        #         else:
+        #             s[i]=''
+        # while stack:
+        #     s[stack.pop()]=''
+        # return ''.join(s)                    
 
         
         

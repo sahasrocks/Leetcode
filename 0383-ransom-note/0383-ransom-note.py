34864ca91ad7c2ef26708c89df7d0162739e14f6
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-04-01 09:17:41 +0530

    Time: 19 ms (60.43%), Space: 19.7 MB (55.38%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,9 +1,27 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        mag=list(magazine)
+        m=list(magazine)
         for i in ransomNote:
-            if i in mag:
-                mag.remove(i)
+            if i in m:
+                m.remove(i)
             else:
                 return False
         return True            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # mag=list(magazine)
+        # for i in ransomNote:
+        #     if i in mag:
+        #         mag.remove(i)
+        #     else:
+        #         return False
+        # return True            

029cd0975ecef9240975ebfb89717153cbb7e362
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-04-01 08:18:10 +0530

    Time: 11 ms (77.06%), Space: 19.3 MB (50.88%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -12,6 +12,32 @@ class Solution:
         return res        
         
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # c=set()
+        # l=0
+        # res=0
+        # for r in range(len(s)):
+        #     while s[r] in c:
+        #         c.remove(s[l])
+        #         l+=1
+        #     c.add(s[r])
+        #     res=max(res,r-l+1)
+        # return res        
+        
+        
         # c=set()
         # l=0
         # res=0

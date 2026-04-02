45e3d4d300dc02e04426cb99ef352bc6a3c83976
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-04-02 06:45:23 +0530

    Time: 113 ms (69.86%), Space: 69.8 MB (16.49%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -0,0 +1,54 @@
+class TimeMap:
+
+    def __init__(self):
+        self.keystore={}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keystore:
+            self.keystore[key]=[]
+        self.keystore[key].append([value,timestamp])
+
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        res,values="",self.keystore.get(key,[])
+        l,r=0,len(values)-1
+        while l<=r:
+            m=(l+r)//2
+            if values[m][1]<=timestamp:
+                res=values[m][0]
+                l=m+1
+            else:
+                r=m-1
+        return res            
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
+
+#class TimeMap:
+#     def __init__(self):
+	
+#         self.values = defaultdict(list)
+#         self.stamps = defaultdict(list)
+    
+#     def set(self, key: str, value: str, timestamp: int) -> None:
+	
+#         self.values[key].append(value)
+#         self.stamps[key].append(timestamp)
+#         return
+        
+#     def get(self, key: str, timestamp: int) -> str:
+	
+#         i = bisect_right(self.stamps[key],timestamp)-1
+#         return '' if i == -1 else self.values[key][i]
+        
+
+
+# # Your TimeMap object will be instantiated and called as such:
+# # obj = TimeMap()
+# # obj.set(key,value,timestamp)
+# # param_2 = obj.get(key,timestamp)

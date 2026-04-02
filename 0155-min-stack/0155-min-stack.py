219384751330bec67eb557b2c85bfbe0249e1000
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-04-02 06:21:35 +0530

    Time: 7 ms (48.21%), Space: 22.5 MB (50.68%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -4,8 +4,6 @@ class MinStack:
         self.stack=[]
         self.min=[]
 
-        
-
     def push(self, val: int) -> None:
         self.stack.append(val)
         val=min(val,self.min[-1] if self.min else val)
@@ -14,11 +12,9 @@ class MinStack:
     def pop(self) -> None:
         self.stack.pop()
         self.min.pop()
-        
 
     def top(self) -> int:
         return self.stack[-1]
-        
 
     def getMin(self) -> int:
         return self.min[-1]
@@ -30,54 +26,3 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
-# class MinStack:
-
-# #     def __init__(self):
-# #         self.stack=[]
-# #         self.min=[]
-
-# #     def push(self, val: int) -> None:
-# #         self.stack.append(val)
-# #         val=min(val,self.min[-1] if self.min else val)
-# #         self.min.append(val)
-        
-
-# #     def pop(self) -> None:
-# #         self.stack.pop()
-# #         self.min.pop()
-        
-
-# #     def top(self) -> int:
-# #         return self.stack[-1]
-        
-
-# #     def getMin(self) -> int:
-# #         return self.min[-1]
-
-
-# # # Your MinStack object will be instantiated and called as such:
-# # # obj = MinStack()
-# # # obj.push(val)
-# # # obj.pop()
-# # # param_3 = obj.top()
-# # # param_4 = obj.getMin()
-# class MinStack:
-
-#     def __init__(self):
-#         self.st = []
-
-#     def push(self, val: int) -> None:
-#         min_val = self.getMin()
-#         if min_val == None or min_val > val:
-#             min_val = val
-        
-#         self.st.append([val, min_val])
-
-#     def pop(self) -> None:
-#         self.st.pop()
-
-#     def top(self) -> int:
-#         return self.st[-1][0] if self.st else None
-
-#     def getMin(self) -> int:
-#         return self.st[-1][1] if self.st else None

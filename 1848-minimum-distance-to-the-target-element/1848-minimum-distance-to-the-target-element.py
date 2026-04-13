423de27da5853f2d1204afc64c9686f41d406b9d
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-04-13 06:15:19 +0530

    Time: 4 ms (5.93%), Space: 19.2 MB (88.72%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.py
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.py
@@ -1,12 +1,17 @@
 class Solution:
     def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
-        left = right = start
-        n = len(nums) - 1
+        # left = right = start
+        # n = len(nums) - 1
 
-        while nums[left] != target and nums[right] != target:
-            if left > 0:
-                left -= 1
-            if right < n:
-                right += 1
+        # while nums[left] != target and nums[right] != target:
+        #     if left > 0:
+        #         left -= 1
+        #     if right < n:
+        #         right += 1
 
-        return max(right - start, start - left)
+        # return max(right - start, start - left)
+        ans=1e9
+        for i in range(len(nums)):
+            if nums[i]==target:
+                ans=min(ans,abs(i-start))
+        return ans        

faa1486b13902e91f677e7c284acd231c792d4df
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-06-09 05:20:16 +0530

    Time: 44 ms (78.11%), Space: 36.7 MB (21.21%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,25 +1,5 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # st = set(nums)    
-        # mx = 0
-        # for num in nums:
-        #     if num-1 not in st:
-        #         tmp = 1
-        #         while num+1 in st:
-        #             tmp += 1
-        #             num += 1
-        #         mx = max(mx, tmp)
-
-        # return mx
-        # numSet=set(nums)
-        # longest=0
-        # for n in numSet:
-        #     if (n-1) not in numSet:
-        #         length=0
-        #         while (n+length) in numSet:
-        #             length+=1
-        #         longest=max(longest,length)
-        # return longest          
         s=set(nums)
         longest=0
         for n in s:
@@ -27,56 +7,21 @@ class Solution:
                 next_n=n+1
                 length=1
                 while next_n in s:
-                    length+=1
                     next_n+=1
+                    length+=1
                 longest=max(longest,length)
         return longest            
-        
-        # s=set(nums)
-        # longest=0
-        # for n in s:
-        #     if n-1 not in s:
-        #         next_n=n+1
-        #         length=1
-        #         while next_n in s:
-        #             length+=1
-        #             next_n+=1
-        #         longest=max(length,longest)
-        # return longest            
-        # s=set(nums)
-        # longest=0
-        # for n in s:
-        #     if n-1 not in s:
-        #         next_n = n+1
-        #         length=1
-        #         while next_n in s:
-        #             length+=1
-        #             next_n+=1
-        #         longest=max(longest,length)
-        # return longest            
-        
-        # s=set(nums)
-        # longest=0
-        # for n in s:
-        #     if n-1 not in s:
-        #         next_n=n+1
-        #         length=1
-        #         while next_n in s:
-        #             length+=1
-        #             next_n+=1
-        #         longest=max(longest,length)
-        # return longest            
-        # s=set(nums)
-        # longest=0
-        # for n in s:
-        #     if n-1 not in s:
-        #         next_n=n+1
-        #         length=1
-        #         while next_n in s:
-        #             length+=1
-        #             next_n +=1
-        #         longest=max(longest,length)    
-        # return longest        
+
+
+
+
+
+
+
+
+
+
+
 
 
         # s=set(nums)

ee752efe2c060dbf3f7ea322b74464477fa01bb1
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-06-10 22:25:45 +0530

    Time: 152 ms (21.05%), Space: 19.6 MB (85.36%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,15 +1,71 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count=defaultdict(int)
-        l=0
-        res=0
+        c=defaultdict(int)
+        l,res=0,0
         for r in range(len(s)):
-            count[s[r]]+=1
-            while (r-l+1) - max(count.values())>k:
-                count[s[l]]-=1
+            c[s[r]]+=1
+            while r-l+1 -max(c.values())>k:
+                c[s[l]]-=1
                 l+=1
             res=max(res,r-l+1)
         return res        
+
+
+
+
+
+
+
+
+
+
+
+
+        
+               
+        # c=defaultdict(int)
+        # l,res=0,0
+        # for r in range(len(s)):
+        #     c[s[r]]+=1
+        #     while (r-l+1)-max(c.values())>k:
+        #         c[s[l]]-=1
+        #         l+=1
+        #     res=max(res,r-l+1)
+        # return res        
+        
+        
+        # c=defaultdict(int)
+        # l=0
+        # res=0
+        # for r in range(len(s)):
+        #     c[s[r]]+=1
+        #     while (r-l+1) - max(c.values())>k:
+        #         c[s[l]]-=1
+        #         l+=1
+        #     res=max(res,r-l+1)
+        # return res        
+        
+        # count=defaultdict(int)
+        # l=0
+        # res=0
+        # for r in range(len(s)):
+        #     count[s[r]]+=1
+        #     while (r-l+1) - max(count.values()) >k:
+        #         count[s[l]]-=1
+        #         l+=1
+        #     res=max(res,r-l+1)
+        # return res        
+
+        # count=defaultdict(int)
+        # l=0
+        # res=0
+        # for r in range(len(s)):
+        #     count[s[r]]+=1
+        #     while (r-l+1) - max(count.values())>k:
+        #         count[s[l]]-=1
+        #         l+=1
+        #     res=max(res,r-l+1)
+        # return res        
 
         
         

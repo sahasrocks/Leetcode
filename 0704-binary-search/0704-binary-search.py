1f229e7257fbb29e3053d9285d83c4c7f1d82e43
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-06-11 05:13:05 +0530

    Time: 0 ms (100%), Space: 20.6 MB (35.33%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,14 +1,80 @@
+#   class Solution:
+#     def search(self, num: List[int], target: int) -> int:
+#         l=0
+#         h=len(num)-1
+        
+#         while l<=h:
+#             m= l + ((h-1)//2)
+#             if num[m] > target:
+#                 h=m-1
+#             elif num[m] < target:
+#                 l=m+1
+#             else:
+#                 return m
+#         return -1    
 class Solution:
-    def search(self, nums: List[int], t: int) -> int:
-        low = 0
-        high = len(nums)-1
-        mid=0
-        while low<=high:
-            mid = (high+low)//2
-            if nums[mid]<t:
-                low = mid+1
-            elif nums[mid]>t:
-                high = mid-1
+    def search(self, nums: List[int], target: int) -> int:
+        l,r=0,len(nums)-1
+        while l<=r:
+            m=(l+r)//2
+            if nums[m]>target:
+                r=m-1
+            elif nums[m]<target:
+                l=m+1
             else:
-                return mid
-        return -1    
+                return m
+        return -1                          
+        
+        
+        # l,r=0,len(nums)-1
+        # while l<=r:
+        #     m=(l+r)//2
+        #     if nums[m]>target:
+        #         r=m-1
+        #     elif nums[m]<target:
+        #         l=m+1
+        #     else:
+        #         return m
+        # return -1                
+        
+        
+        # l,r=0,len(nums)-1
+        # while l<=r:
+        #     m=l+(r-l)//2
+        #     if nums[m]>target:
+        #         r=m-1
+        #     elif nums[m]<target:
+        #         l=m+1
+        #     else:
+        #         return m
+        # return -1                
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # l, r = 0, len(nums) - 1
+
+        # while l <= r:
+        #     # (l + r) // 2 can lead to overflow
+        #     m = l + ((r - l) // 2)  
+
+        #     if nums[m] > target:
+        #         r = m - 1
+        #     elif nums[m] < target:
+        #         l = m + 1
+        #     else:
+        #         return m
+        # return -1          
+            
+        

ad24ed96c34cb4e438f39ebc4202298dda77bc6b
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-06-10 06:04:58 +0530

    Time: 0 ms (100%), Space: 19.3 MB (73.03%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,15 +1,65 @@
-class Solution(object):
-    def longestCommonPrefix(self, a):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        res=""
+class Solution:
+    def longestCommonPrefix(self, a: List[str]) -> str:
+        # res=''
+        # for i in range(len(a[0])):
+        #     for s in a:
+        #         if i==len(s) or s[i] != a[0][i]:
+        #             return res
+        #     res+=a[0][i]
+        # return res                    
+        
+        res=''
         for i in range(len(a[0])):
             for s in a:
                 if i==len(s) or s[i] !=a[0][i]:
                     return res
-            res+=a[0][i]
-        return res            
+            res+=a[0][i] 
+        return res           
         
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # s = len(a)
+        # if s==0:
+        #     return ""
+        # if s ==1:
+        #     return a[0]
+        
+        # a.sort()
+        # end = min(len(a[0]), len(a[s - 1]))
+        # i = 0
+        # while (i < end and
+        #        a[0][i] == a[s - 1][i]):
+        #     i += 1
+
+        # pre = a[0][0: i]
+        # return pre
+        # res = ""
+        # for i in range(len(a[0])):
+        #     for s in a:
+        #         if i ==len(s) or s[i] != a[0][i]:
+        #             return res
+        #     res += a[0][i]
+        # return res           
+        # res=""
+        # for i in range(len(a[0])):
+        #     for s in a:
+        #         if i==len(s) or s[i] !=a[0][i]:
+        #             return res
+        #     res+=a[0][i]
+        # return res            
+
+
+
+
+

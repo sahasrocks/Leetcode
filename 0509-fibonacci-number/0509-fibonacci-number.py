1a39655471d6401a43cd5a5f6b6511e5756c8247
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-06-16 21:38:49 +0530

    Time: 51 ms (47.67%), Space: 19.1 MB (57.38%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -16,6 +16,30 @@ class Solution:
         for i in range(2,n+1):
             dp[i]=dp[i-1]+dp[i-2]
         return dp[n]        
+        
+        
+        
+        
+        
+        
+        
+        
+        # if n<=1:
+        #     return n
+        # dp=[-1]*(n+1)
+        # dp[0]=0
+        # dp[1]=1
+        # for i in range(2,n+1):
+        #     dp[i]=dp[i-1]+dp[i-2]
+        # return dp[n]        
+        # if n<=1:
+        #     return n
+        # dp=[-1]*(n+1)
+        # dp[0]=0
+        # dp[1]=1
+        # for i in range(2,n+1):
+        #     dp[i]=dp[i-1]+dp[i-2]
+        # return dp[n]        
         # if n==0:
         #     return 0
         # if n==1:

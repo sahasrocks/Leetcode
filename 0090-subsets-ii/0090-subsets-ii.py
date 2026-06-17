07737347bfcaf7f84a1046c5b97332d4c4b0a701
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-06-17 09:18:31 +0530

    Time: 1 ms (26.63%), Space: 19.6 MB (23%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -16,6 +16,22 @@ class Solution:
         return res            
         
         
+        # res=[]
+        # nums.sort()
+        # def dfs(i,subset):
+        #     if i==len(nums):
+        #         res.append(subset.copy())
+        #         return
+        #     subset.append(nums[i])
+        #     dfs(i+1,subset)
+        #     subset.pop()
+        #     while i+1<len(nums) and nums[i]==nums[i+1]:
+        #         i+=1
+        #     dfs(i+1,subset)
+        # dfs(0,[])
+        # return res            
+        
+        
         # res = []
         # nums.sort()
 

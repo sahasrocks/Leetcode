deeec9228958f4b8fce4f1381c9afd0a89949e03
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-06-17 07:57:32 +0530

    Time: 0 ms (100%), Space: 19.3 MB (67.3%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -11,7 +11,24 @@ class Solution:
             subset.pop()
             dfs(i+1)
         dfs(0)
-        return res              
+        return res        
+        
+        
+        
+        
+        
+        # res=[]
+        # subset=[]
+        # def dfs(i):
+        #     if i==len(nums):
+        #         res.append(subset.copy())
+        #         return
+        #     subset.append(nums[i])
+        #     dfs(i+1)
+        #     subset.pop()
+        #     dfs(i+1)
+        # dfs(0)
+        # return res              
         
         
         # res = []

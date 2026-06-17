7fd01eff5567da00f630fa644c7f7b836598a327
Sahas Lakhote <86784319+sahasrocks@users.noreply.github.com>
2026-06-17 09:59:49 +0530

    Time: 3 ms (3.76%), Space: 19.1 MB (99.41%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -12,12 +12,55 @@ class Solution:
 
         res=[]
         digitToChar = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
-        def backtrack(i,curStr):
-            if len(curStr)==len(digits):
-                res.append(curStr)
+        def dfs(i,curS):
+            if len(curS)==len(digits):
+                res.append(curS)
                 return
             for c in digitToChar[digits[i]]:
-                backtrack(i+1,curStr+c)
+                dfs(i+1,curS+c)
         if digits:
-            backtrack(0,"")
-        return res                
+            dfs(0,'')
+        return res            
+        
+        
+        
+        # def dfs(i,curS):
+        #     if len(curS)==len(digits):
+        #         res.append(curS)
+        #         return
+        #     for c in digitToChar[digits[i]]:
+        #         dfs(i+1,curS+c)
+        # if digits:
+        #     dfs(0,"")
+        # return res    
+
+        # def dfs(i,curStr):
+
+        #     if len(curStr)==len(digits):
+        #         res.append(curStr)
+        #         return
+        #     for c in digitToChar[digits[i]]:
+        #         dfs(i+1,curStr+c)
+        # if digits:
+        #     dfs(0,"")
+        # return res                
+        
+        # def bt(i,curStr):
+        #     if len(curStr)==len(digits):
+        #         res.append(curStr)
+        #         return
+        #     for c in  digitToChar[digits[i]]:
+        #         bt(i+1,curStr+c)
+        # if digits:
+        #     bt(0,"")
+        # return res           
+        
+        # def backtrack(i,curStr):
+        #     if len(curStr)==len(digits):
+        #         res.append(curStr)
+        #         return
+        #     for c in digitToChar[digits[i]]:
+        #         backtrack(i+1,curStr+c)
+        # if digits:
+        #     backtrack(0,"")
+        # return res                
